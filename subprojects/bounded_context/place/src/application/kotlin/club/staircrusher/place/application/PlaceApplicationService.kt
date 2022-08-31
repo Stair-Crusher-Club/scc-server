28944376ea7421e1ab01f9b7efb00e4c2de24fbd
bellatoris <bellatoris@snu.ac.kr>
2022-08-31 22:45:15 +0900

    implement PlaceApplicationService
    Client can search places with multiple parameters (keyword, address,
    category) and filters (accessbility, radius from location, category).
    So `PlaceApplicationService` should implement all these use cases, and in
    order to support use case *searching only places where accessibility
    information is not registered* we should cache the third party's search
    results in our database. If not, third party api can be requested over
    and over again in certain situation wherer accessibility information of
    almost of places returned by requests are already registered.

--- subprojects/bounded_context/place/src/application/kotlin/club/staircrusher/place/application/PlaceApplicationService.kt
+++ subprojects/bounded_context/place/src/application/kotlin/club/staircrusher/place/application/PlaceApplicationService.kt
@@ -2,11 +2,24 @@ package club.staircrusher.place.application
 
 import club.staircrusher.place.domain.entity.Place
 import club.staircrusher.place.domain.repository.PlaceRepository
+import club.staircrusher.place.domain.service.MapsService
+import club.staircrusher.stdlib.persistence.TransactionManager
 
 class PlaceApplicationService(
     private val placeRepository: PlaceRepository,
+    private val mapsService: MapsService,
+    private val transactionManager: TransactionManager,
 ) {
     fun findPlace(placeId: String): Place? {
         return placeRepository.findByIdOrNull(placeId)
+    }
+
+    // FIXME: Should this function be suspend?
+    suspend fun findByKeyword(keyword: String): List<Place> {
+        val places = mapsService.findByKeyword(keyword)
+        transactionManager.doInTransaction {
+            placeRepository.saveAll(places)
+        }
+        return places
     }
 }
