00902069c7e3c2c46a73d1581c5b27c0fc8f19c6
bellatoris <bellatoris@snu.ac.kr>
2022-09-14 22:57:29 +0900

    addressed comments

--- subprojects/stdlib/src/main/kotlin/club/staircrusher/stdlib/geography/Length.kt
+++ subprojects/stdlib/src/main/kotlin/club/staircrusher/stdlib/geography/Length.kt
@@ -1,11 +1,11 @@
 package club.staircrusher.stdlib.geography
 
 data class Length(
-   private val meter: Double
+   private val meter: Double,
 ) {
     companion object {
         fun ofMeters(meters: Double) = Length(meters)
-        fun ofKiloMeters(kiloMeters: Double) = Length(kiloMeters * 1000)
+        fun ofKilometers(kilometers: Double) = Length(kilometers * 1000)
     }
 
     operator fun plus(other: Length) = Length(meter + other.meter)
