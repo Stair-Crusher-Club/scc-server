c4d7ef84ba4bf5c431d91583c08e68873a7e52ad
bellatoris <bellatoris@snu.ac.kr>
2022-09-14 23:15:30 +0900

    fix method signature correctly

--- subprojects/bounded_context/place/src/output-adapter/kotlin/club/staircrusher/place/output_adapter/service/KakaoMapsService.kt
+++ subprojects/bounded_context/place/src/output-adapter/kotlin/club/staircrusher/place/output_adapter/service/KakaoMapsService.kt
@@ -3,10 +3,9 @@ package club.staircrusher.place.output_adapter.service
 import club.staircrusher.place.domain.entity.Place
 import club.staircrusher.place.domain.entity.PlaceCategory
 import club.staircrusher.place.domain.service.MapsService
-import club.staircrusher.stdlib.geography.Location
 
 class KakaoMapsService: MapsService {
-    override suspend fun findByAddress(location: Location): List<Place> {
+    override suspend fun findByAddress(address: String): List<Place> {
         TODO("Not yet implemented")
     }
 
