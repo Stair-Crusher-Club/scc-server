6bd959a02236254b3505ffc5130a20321ca7cc17
bellatoris <bellatoris@snu.ac.kr>
2022-09-21 23:36:03 +0900

    implement kakaoApi with http interface

--- subprojects/bounded_context/place/build.gradle.kts
+++ subprojects/bounded_context/place/build.gradle.kts
@@ -10,5 +10,11 @@ dependencyManagement {
 }
 
 dependencies {
+    val kotlinLoggingVersion: String by project
+
     outputAdapterImplementation("org.springframework.boot:spring-boot-starter-web")
+    outputAdapterImplementation("org.springframework:spring-webflux")
+    outputAdapterImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+    outputAdapterImplementation("io.github.microutils:kotlin-logging-jvm:$kotlinLoggingVersion")
+    outputAdapterImplementation("io.projectreactor.netty:reactor-netty")
 }
