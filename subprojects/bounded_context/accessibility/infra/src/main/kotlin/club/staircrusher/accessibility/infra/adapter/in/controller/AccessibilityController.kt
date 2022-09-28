c9edf200aefb9b5e2a66729e271e145a6134c67d
Zeniuus <jsh56son@gmail.com>
2022-09-28 23:20:43 +0900

    /registerAccessibility 구현

--- subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AccessibilityController.kt
+++ subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AccessibilityController.kt
@@ -1,9 +1,14 @@
 package club.staircrusher.accessibility.infra.adapter.`in`.controller
 
 import club.staircrusher.accessibility.application.AccessibilityApplicationService
+import club.staircrusher.accessibility.domain.service.BuildingAccessibilityCommentService
+import club.staircrusher.accessibility.domain.service.PlaceAccessibilityCommentService
 import club.staircrusher.accessibility.infra.adapter.`in`.converter.toDTO
+import club.staircrusher.accessibility.infra.adapter.`in`.converter.toModel
 import club.staircrusher.api.spec.dto.GetAccessibilityPost200Response
 import club.staircrusher.api.spec.dto.GetAccessibilityPostRequest
+import club.staircrusher.api.spec.dto.RegisterAccessibilityPost200Response
+import club.staircrusher.api.spec.dto.RegisterAccessibilityPostRequest
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -28,6 +33,47 @@ class AccessibilityController(
             buildingAccessibilityComments = result.buildingAccessibilityComments.map { it.toDTO(user = null) },
             placeAccessibilityComments = result.placeAccessibilityComments.map { it.toDTO(user = null) },
             hasOtherPlacesToRegisterInBuilding = result.hasOtherPlacesToRegisterInSameBuilding,
+        )
+    }
+
+    @PostMapping("/registerAccessibility")
+    fun registerAccessibility(@RequestBody request: RegisterAccessibilityPostRequest): RegisterAccessibilityPost200Response {
+        // TODO: Spring Security를 통해 access token의 userId 추출해서 사용해야 함.
+        // TODO: upvote 정보 및 유저 정보 제대로 채우기
+        val result = accessibilityApplicationService.register(
+            createBuildingAccessibilityParams = request.buildingAccessibilityParams?.toModel(userId = ""),
+            createBuildingAccessibilityCommentParams = request.buildingAccessibilityParams?.comment?.let {
+                BuildingAccessibilityCommentService.CreateParams(
+                    buildingId = request.buildingAccessibilityParams!!.buildingId,
+                    userId = null,
+                    comment = it,
+                )
+            },
+            createPlaceAccessibilityParams = request.placeAccessibilityParams.toModel(userId = ""),
+            createPlaceAccessibilityCommentParams = request.placeAccessibilityParams.comment?.let {
+                PlaceAccessibilityCommentService.CreateParams(
+                    placeId = request.placeAccessibilityParams.placeId,
+                    userId = null,
+                    comment = it,
+                )
+            },
+        )
+        return RegisterAccessibilityPost200Response(
+            buildingAccessibility = result.buildingAccessibility?.toDTO(
+                isUpvoted = false,
+                totalUpvoteCount = 0,
+                registeredUserName = null,
+            ),
+            buildingAccessibilityComments = listOfNotNull(result.buildingAccessibilityComment).map {
+                it.toDTO(user = null)
+            },
+            placeAccessibility = result.placeAccessibility.toDTO(
+                registeredUserName = null,
+            ),
+            placeAccessibilityComments = listOfNotNull(result.placeAccessibilityComment).map {
+                it.toDTO(user = null)
+            },
+            registeredUserOrder = 0,
         )
     }
 }
---
+++ subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/converter/PlaceAccessibilityRegisterParamsConverter.kt
@@ -0,0 +1,13 @@
+package club.staircrusher.accessibility.infra.adapter.`in`.converter
+
+import club.staircrusher.accessibility.domain.service.PlaceAccessibilityService
+import club.staircrusher.api.spec.dto.RegisterAccessibilityPostRequestPlaceAccessibilityParams
+
+
+fun RegisterAccessibilityPostRequestPlaceAccessibilityParams.toModel(userId: String) = PlaceAccessibilityService.CreateParams(
+    placeId = placeId,
+    isFirstFloor = isFirstFloor,
+    stairInfo = stairInfo.toModel(),
+    hasSlope = hasSlope,
+    userId = userId,
+)
