1cc14e4bea54413b6e359c7e2a80f33bbf05b749
bellatoris <bellatoris@snu.ac.kr>
2022-10-06 22:23:53 +0900

    surround event listener with try-catch

--- subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/out/message/SpringEventListener.kt
+++ subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/out/message/SpringEventListener.kt
@@ -2,13 +2,23 @@ package club.staircrusher.place.infra.adapter.out.message
 
 import club.staircrusher.stdlib.annotation.Component
 import club.staircrusher.stdlib.domain.event.DomainEventListener
+import mu.KotlinLogging
 import org.springframework.context.ApplicationListener
 
 @Component
 class SpringEventListener(
     private val domainEventListeners: List<DomainEventListener<*>>,
 ): ApplicationListener<DomainSpringEvent> {
+    private val logger = KotlinLogging.logger {}
     override fun onApplicationEvent(event: DomainSpringEvent) {
-        domainEventListeners.forEach { it(event.domainEvent) }
+        domainEventListeners.forEach {
+            try {
+                it(event.domainEvent)
+            } catch (e: Throwable) {
+                logger.error(e) {
+
+                }
+            }
+        }
     }
 }
