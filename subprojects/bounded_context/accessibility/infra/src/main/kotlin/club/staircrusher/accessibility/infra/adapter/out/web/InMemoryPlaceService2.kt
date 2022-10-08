704e1898776fd0014b95fee053a21b8dd9da5b7b
bellatoris <bellatoris@snu.ac.kr>
2022-10-08 22:21:06 +0900

    resolve conflicts of name of beans

--- subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/out/web/InMemoryPlaceService2.kt
+++ subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/out/web/InMemoryPlaceService2.kt
@@ -4,8 +4,11 @@ import club.staircrusher.accessibility.domain.model.Place
 import club.staircrusher.accessibility.domain.service.PlaceService
 import org.springframework.stereotype.Component
 
+/**
+ * Add suffix 2 in order to avoid conflicts with another InMemoryPlaceService
+ */
 @Component
-class InMemoryPlaceService(
+class InMemoryPlaceService2(
     private val placeService: club.staircrusher.place.application.service.PlaceService,
 ) : PlaceService {
     override fun findPlace(placeId: String): Place? {
