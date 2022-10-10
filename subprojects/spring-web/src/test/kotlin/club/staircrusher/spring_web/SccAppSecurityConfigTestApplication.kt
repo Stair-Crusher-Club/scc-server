4e1d084b0c046295be4f355a39cf9f9274c36934
Zeniuus <jsh56son@gmail.com>
2022-10-10 21:56:19 +0900

    빌드 오류 수정

--- subprojects/spring-web/src/test/kotlin/club/staircrusher/spring_web/SccAppSecurityConfigTestApplication.kt
+++ subprojects/spring-web/src/test/kotlin/club/staircrusher/spring_web/SccAppSecurityConfigTestApplication.kt
@@ -1,10 +1,19 @@
 package club.staircrusher.spring_web
 
+import club.staircrusher.stdlib.di.annotation.Component
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import java.time.Clock
 
-@SpringBootApplication(scanBasePackages = ["club.staircrusher"])
+@SpringBootApplication(proxyBeanMethods = false)
+@ComponentScan(
+    basePackages = ["club.staircrusher"],
+    includeFilters = [
+        ComponentScan.Filter(type = FilterType.ANNOTATION, classes = [Component::class]),
+    ],
+)
 open class SccAppSecurityConfigTestApplication {
     @Bean
     open fun clock(): Clock {
