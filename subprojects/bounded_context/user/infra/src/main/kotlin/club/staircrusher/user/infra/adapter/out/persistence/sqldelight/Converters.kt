9fc247db2dda75ea4b88990678d9cc89371cabc0
Zeniuus <jsh56son@gmail.com>
2022-10-10 17:38:26 +0900

    user bc에 sqldelight 붙이기

---
+++ subprojects/bounded_context/user/infra/src/main/kotlin/club/staircrusher/user/infra/adapter/out/persistence/sqldelight/Converters.kt
@@ -0,0 +1,25 @@
+package club.staircrusher.user.infra.adapter.out.persistence.sqldelight
+
+import club.staircrusher.infra.persistence.sqldelight.user.User
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+fun User.toDomainModel() = club.staircrusher.user.domain.entity.User(
+    id = id,
+    nickname = nickname,
+    encryptedPassword = encrypted_password,
+    instagramId = instagram_id,
+    createdAt = created_at.toInstant(),
+)
+
+fun club.staircrusher.user.domain.entity.User.toPersistenceModel() = User(
+    id = id,
+    nickname = nickname,
+    encrypted_password = encryptedPassword,
+    instagram_id = instagramId,
+    created_at = createdAt.toLocalDateTime(),
+)
+
+private fun LocalDateTime.toInstant() = toInstant(ZoneOffset.UTC)
+private fun Instant.toLocalDateTime() = atOffset(ZoneOffset.UTC).toLocalDateTime()
