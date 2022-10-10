1a1a1da4dd336e1064bcdc85e3a3d7192e16972f
Zeniuus <jsh56son@gmail.com>
2022-10-10 21:56:19 +0900

    빌드 오류 수정

---
+++ subprojects/spring-web/src/test/kotlin/club/staircrusher/spring_web/NoopPasswordEncryptor.kt
@@ -0,0 +1,15 @@
+package club.staircrusher.spring_web
+
+import club.staircrusher.stdlib.di.annotation.Component
+import club.staircrusher.user.domain.service.PasswordEncryptor
+
+@Component
+class NoopPasswordEncryptor : PasswordEncryptor {
+    override fun encrypt(password: String): String {
+        return password
+    }
+
+    override fun verify(inputPassword: String, targetPassword: String): Boolean {
+        return inputPassword == targetPassword
+    }
+}
