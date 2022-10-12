7c9b1f30670e7a1769768cc82b558bd388e3bcf5
bellatoris <bellatoris@snu.ac.kr>
2022-10-12 20:35:23 +0900

    add KopyKat dependencies

--- subprojects/stdlib/build.gradle.kts
+++ subprojects/stdlib/build.gradle.kts
@@ -1,4 +1,16 @@
+plugins {
+    id("com.google.devtools.ksp")
+}
+
 dependencies {
     val geoToolsVersion: String by project
+    val kopyKatVersion: String by project
     implementation("org.geotools:gt-referencing:$geoToolsVersion")
+
+    ksp("at.kopyk:kopykat-ksp:$kopyKatVersion")
+    compileOnly("at.kopyk:kopykat-annotations:$kopyKatVersion")
+}
+
+ksp {
+    arg("generate", "annotated")
 }
