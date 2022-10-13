ba02c2472950e4a17f8d81c290af09b9ac11959d
bellatoris <bellatoris@snu.ac.kr>
2022-10-13 16:59:47 +0900

    implement isolation level modifiable tx manager

--- subprojects/persistence_model/src/main/kotlin/club/staircrusher/infra/persistence/sqldelight/DB.kt
+++ subprojects/persistence_model/src/main/kotlin/club/staircrusher/infra/persistence/sqldelight/DB.kt
@@ -1,14 +1,18 @@
 package club.staircrusher.infra.persistence.sqldelight
 
 import app.cash.sqldelight.ColumnAdapter
+import app.cash.sqldelight.TransacterImpl
 import app.cash.sqldelight.driver.jdbc.asJdbcDriver
 import club.staircrusher.infra.persistence.sqldelight.migration.Place
 import club.staircrusher.stdlib.di.annotation.Component
+import club.staircrusher.stdlib.persistence.Transaction
+import club.staircrusher.stdlib.persistence.TransactionIsolationLevel
+import club.staircrusher.stdlib.persistence.TransactionManager
 import club.staircrusher.stdlib.place.PlaceCategory
 import javax.sql.DataSource
 
 @Component
-class DB(dataSource: DataSource) {
+class DB(dataSource: DataSource) : TransactionManager, TransacterImpl(JdbcDriver(dataSource)) {
     private val placeCategoryStringColumnAdapter = object : ColumnAdapter<PlaceCategory, String> {
         override fun decode(databaseValue: String): PlaceCategory {
             return PlaceCategory.valueOf(databaseValue)
@@ -17,7 +21,7 @@ class DB(dataSource: DataSource) {
         override fun encode(value: PlaceCategory): String = value.name
     }
 
-    internal val scc = scc(
+    private val scc = scc(
         driver = dataSource.asJdbcDriver(),
         placeAdapter = Place.Adapter(
             categoryAdapter = placeCategoryStringColumnAdapter,
@@ -32,4 +36,38 @@ class DB(dataSource: DataSource) {
     val placeAccessibilityQueries = scc.placeAccessibilityQueries
     val placeAccessibilityCommentQueries = scc.placeAccessibilityCommentQueries
     val userQueries = scc.userQueries
+
+    override fun <T> doInTransaction(block: Transaction<T>.() -> T): T {
+        val driver = this.driver as JdbcDriver
+        check(driver.isolationLevel == null) {
+            """
+            Since SCC does not allow nested transaction, isolationLevel saved in
+            thread local must be null.
+            """.trimIndent()
+        }
+        return transactionWithResult(noEnclosing = true) {
+            Transaction(this).block()
+        }
+    }
+
+    override fun <T> doInTransaction(
+        isolationLevel: TransactionIsolationLevel,
+        block: Transaction<T>.() -> T,
+    ): T {
+        val driver = this.driver as JdbcDriver
+        check(driver.isolationLevel == null) {
+            """
+            Since SCC does not allow nested transaction, isolationLevel saved in
+            thread local must be null.
+            """.trimIndent()
+        }
+        driver.isolationLevel = isolationLevel.toConnectionIsolationLevel()
+        return try {
+            transactionWithResult(noEnclosing = true) {
+                Transaction(this).block()
+            }
+        } finally {
+            driver.isolationLevel = null
+        }
+    }
 }
--- subprojects/stdlib/src/main/kotlin/club/staircrusher/stdlib/persistence/TransactionIsolationLevel.kt
+++ subprojects/stdlib/src/main/kotlin/club/staircrusher/stdlib/persistence/TransactionIsolationLevel.kt
@@ -3,6 +3,9 @@ package club.staircrusher.stdlib.persistence
 import java.sql.Connection
 
 enum class TransactionIsolationLevel {
+    READ_COMMITTED {
+        override fun toConnectionIsolationLevel() = Connection.TRANSACTION_READ_COMMITTED
+    },
     REPEATABLE_READ {
         override fun toConnectionIsolationLevel() = Connection.TRANSACTION_REPEATABLE_READ
     },
