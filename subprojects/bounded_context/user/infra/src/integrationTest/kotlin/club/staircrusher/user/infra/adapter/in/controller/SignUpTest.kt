f95faefb61c66761b428e45a0dcf721070de205c
Zeniuus <jsh56son@gmail.com>
2022-10-16 18:41:41 +0900

    user bc에 테스트 옮기기

---
+++ subprojects/bounded_context/user/infra/src/integrationTest/kotlin/club/staircrusher/user/infra/adapter/in/controller/SignUpTest.kt
@@ -0,0 +1,26 @@
+package club.staircrusher.user.infra.adapter.`in`.controller
+
+import club.staircrusher.api.spec.dto.SignUpPostRequest
+import club.staircrusher.spring_web.authentication.SccSecurityFilterChainConfig
+import club.staircrusher.user.infra.adapter.`in`.controller.base.UserITBase
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class SignUpTest : UserITBase() {
+    @Test
+    fun testSignUp() {
+        val nickname = Random.nextBytes(32).toString()
+        val params = SignUpPostRequest(
+            nickname = nickname,
+            instagramId = "instagramId",
+            password = "password",
+        )
+        mvc
+            .sccRequest("/signUp", params)
+            .andExpect {
+                header {
+                    exists(SccSecurityFilterChainConfig.accessTokenHeader)
+                }
+            }
+    }
+}
---
+++ subprojects/bounded_context/user/infra/src/integrationTest/kotlin/club/staircrusher/user/infra/adapter/in/controller/base/UserITBase.kt
@@ -0,0 +1,5 @@
+package club.staircrusher.user.infra.adapter.`in`.controller.base
+
+import club.staircrusher.testing.spring_it.base.SccSpringITBase
+
+open class UserITBase : SccSpringITBase()
