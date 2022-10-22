6a21e3102490337be98d45d975fd7539665f06d4
Zeniuus <jsh56son@gmail.com>
2022-10-22 16:21:25 +0900

    리뷰: 확장자명 검사

--- subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/GetImageUploadUrlsUseCase.kt
+++ subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/GetImageUploadUrlsUseCase.kt
@@ -15,6 +15,10 @@ class GetImageUploadUrlsUseCase(
         if (urlCount > urlCountLimit) {
             throw IllegalArgumentException("한 번에 최대 ${urlCountLimit}개의 URL만 생성할 수 있습니다.")
         }
+        if (filenameExtension !in allowedFilenameExtensions) {
+            throw IllegalArgumentException("${filenameExtension}은 잘못된 확장자입니다. " +
+                "$allowedFilenameExtensions 중 하나의 확장자를 지정해주세요.")
+        }
         return (0 until urlCount).map {
             fileManagementService.getFileUploadUrl(filenameExtension)
         }
@@ -22,5 +26,6 @@ class GetImageUploadUrlsUseCase(
 
     companion object {
         private const val urlCountLimit = 10
+        private val allowedFilenameExtensions = setOf("png", "jpg", "jpeg")
     }
 }
