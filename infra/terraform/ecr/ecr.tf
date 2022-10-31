b86e0cecee413d1bed6cf9e23ca61da9e999ed8a
Zeniuus <jsh56son@gmail.com>
2022-10-31 21:10:46 +0900

    scc-admin-frontend ecr repository 추가

--- infra/terraform/ecr/ecr.tf
+++ infra/terraform/ecr/ecr.tf
@@ -6,3 +6,10 @@ resource "aws_ecrpublic_repository" "scc_server" {
   }
 }
 
+resource "aws_ecrpublic_repository" "scc_admin_frontend" {
+  repository_name = "scc-admin-frontend"
+  catalog_data {
+    architectures     = ["ARM", "x86-64"]
+    operating_systems = ["Linux"]
+  }
+}
