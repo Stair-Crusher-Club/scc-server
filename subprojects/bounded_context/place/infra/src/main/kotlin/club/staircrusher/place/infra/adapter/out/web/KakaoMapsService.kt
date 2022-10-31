618cae211e11edfe3f0f86c2289700d287f9f261
Suhwan Jee <jsh56son@gmail.com>
2022-10-31 19:26:12 +0900

    장소 검색 관련 버그들 수정 (#87)
    * 중복 제거
    
    * Fix bug: 카카오 지도 API 사용 시 페이지네이션 제대로 호출하기
    
    * categoryGroupCode가 null이지만 유효한 점포도 있다
    
    * empty string으로의 장소 검색 막기
    
    * 점포 검색 시 카카오 지도 API의 모든 페이지 결과를 모아서 내려주기
    
    * 테스트용 API script 추가

--- subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/out/web/KakaoMapsService.kt
+++ subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/out/web/KakaoMapsService.kt
@@ -108,7 +108,7 @@ class KakaoMapsService(
     override suspend fun findByKeyword(keyword: String): List<Place> {
         val result = kakaoService.searchByKeyword(keyword).awaitFirstOrNull()
         logger.debug { result }
-        return result?.convertToModel() ?: emptyList()
+        return (result?.convertToModel() ?: emptyList()).removeDuplicates()
     }
 
     override suspend fun findByCategory(category: PlaceCategory): List<Place> {
@@ -121,7 +121,7 @@ class KakaoMapsService(
         var nextPage = 1
         var pageablePage: Int? = null
         while (pageablePage == null || nextPage <= pageablePage) {
-            val apiResult = kakaoService.searchByKeyword(keyword).awaitFirstOrNull()
+            val apiResult = kakaoService.searchByKeyword(keyword, page = nextPage).awaitFirstOrNull()
             logger.debug { apiResult }
             if (apiResult == null) {
                 break
@@ -130,7 +130,7 @@ class KakaoMapsService(
             nextPage += 1
             result += apiResult.convertToModel()
         }
-        return result
+        return result.removeDuplicates()
     }
 
     // TODO: 한 번에 한 페이지만 조회하는 대신 동시에 여러 페이지 조회하기
@@ -147,6 +147,7 @@ class KakaoMapsService(
                         x = region.centerLocation.lng.toString(),
                         y = region.centerLocation.lat.toString(),
                         radius = region.radiusMeters,
+                        page = nextPage,
                     )
                 }
                 is MapsService.SearchOption.RectangleRegion -> {
@@ -166,40 +167,39 @@ class KakaoMapsService(
             nextPage += 1
             result += apiResult.convertToModel()
         }
-        return result
+        return result.removeDuplicates()
     }
 
     private fun SearchResult.convertToModel(): List<Place> {
-        return documents.mapNotNull {
-            if (it.categoryGroupCode == null) {
-                return@mapNotNull null // 운중천과 같이 점포가 아닌 곳도 내려온다. 이런 경우를 필터링해준다.
-            }
-            @Suppress("TooGenericExceptionCaught", "SwallowedException")
-            try {
-                Place(
-                    id = it.id,
-                    name = it.placeName,
-                    location = it.location,
-                    building = Building(
-                        id = Hashing.getHash(
-                            it.roadAddressName,
-                            length = 36
-                        ), // TODO: 정책 제대로 정하기; 근데 어차피 주소로 unique key를 만들어내긴 해야 할 듯.
-                        name = it.roadAddressName,
+        return documents
+            .filterNot { it.isNonPlaceResult() }
+            .mapNotNull {
+                @Suppress("TooGenericExceptionCaught", "SwallowedException")
+                try {
+                    Place(
+                        id = it.id,
+                        name = it.placeName,
                         location = it.location,
-                        address = it.parseToBuildingAddress(),
-                        siGunGuId = "temp", // TODO: 제대로 채우기
-                        eupMyeonDongId = "temp",
-                    ),
-                    siGunGuId = null,
-                    eupMyeonDongId = null,
-                    category = it.categoryGroupCode?.toPlaceCategory(),
-                )
-            } catch (t: Throwable) {
-                logger.warn { "Cannot convert document to model: $it" }
-                null
+                        building = Building(
+                            id = Hashing.getHash(
+                                it.roadAddressName,
+                                length = 36
+                            ), // TODO: 정책 제대로 정하기; 근데 어차피 주소로 unique key를 만들어내긴 해야 할 듯.
+                            name = it.roadAddressName,
+                            location = it.location,
+                            address = it.parseToBuildingAddress(),
+                            siGunGuId = "temp", // TODO: 제대로 채우기
+                            eupMyeonDongId = "temp",
+                        ),
+                        siGunGuId = null,
+                        eupMyeonDongId = null,
+                        category = it.categoryGroupCode?.toPlaceCategory(),
+                    )
+                } catch (t: Throwable) {
+                    logger.warn(t) { "Cannot convert document to model: $it" }
+                    null
+                }
             }
-        }
     }
 
     @Serializable
@@ -319,6 +319,14 @@ class KakaoMapsService(
                     }
                 }
             }
+
+            /**
+             * 일부 검색 결과는 점포가 아닌 경우가 있다. 해당 경우들은 검색 결과로 보여주지 않아야 한다.
+             * - case 1. "운중천" - roadAddressName이 empty string으로 내려온다.
+             */
+            fun isNonPlaceResult(): Boolean {
+                return roadAddressName.isBlank()
+            }
         }
 
         @Serializable
@@ -346,9 +354,6 @@ class KakaoMapsService(
 
     @Suppress("MagicNumber")
     fun SearchResult.Document.parseToBuildingAddress(): BuildingAddress {
-        checkNotNull(categoryGroupCode) {
-            "Should not reach here! Strange search result: $this"
-        }
         val addressNameTokens = addressName.split(" ")
         val siDo = addressNameTokens[0]
         val siGunGu = addressNameTokens[1]
@@ -380,4 +385,8 @@ class KakaoMapsService(
         @SerialName("msg")
         val msg: String,
     )
+
+    private fun List<Place>.removeDuplicates(): List<Place> {
+        return associateBy { it.id }.values.toList()
+    }
 }
