e4a7f54feacd3e791b2c6ec05f74044e9b2f63f4
Zeniuus <jsh56son@gmail.com>
2022-10-31 21:16:27 +0900

    이미지 푸시 스크립트 작성

---
+++ admin-frontend/docker-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $1 != "dev" && $1 != "prod" ]]; then
+  echo 'Usage: ./docker-push.sh (dev|prod)'
+  exit 0
+fi;
+
+IMAGE_TAG=latest
+if [[ $1 = "dev" ]]; then
+  IMAGE_TAG="$IMAGE_TAG-rc"
+fi;
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/q0g6g7m8
+
+npm install
+npm run "build:$1"
+docker buildx build --platform linux/amd64 -t public.ecr.aws/q0g6g7m8/scc-admin-frontend:latest .
+docker push public.ecr.aws/q0g6g7m8/scc-admin-frontend:latest
