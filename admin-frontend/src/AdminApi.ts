53d70d86488948ba0958bb5dadbec01cb09b1c0b
Zeniuus <jsh56son@gmail.com>
2022-11-04 15:44:17 +0900

    에러 핸들링을 서버가 내려주는 것과 맞추기

--- admin-frontend/src/AdminApi.ts
+++ admin-frontend/src/AdminApi.ts
@@ -20,18 +20,20 @@ globalAxios.interceptors.request.use((request) => {
 
 globalAxios.interceptors.response.use((response) => response, (error) => {
   const res = error.response;
-  if (res.status === 401 && getSavedAccessToken() != null) {
+  if (res.status === 401) {
+    alert('잘못된 인증 정보입니다. 로그인 화면으로 이동합니다.');
     clearSavedAccessToken();
+    window.location.pathname = '/login';
+    return;
   }
   if (res == null) {
     alert('알 수 없는 문제가 발생했습니다.');
     throw error;
   }
-  const msg = res.data?.msg;
-  if (msg == null) {
-    alert('알 수 없는 문제가 발생했습니다.');
+  if (typeof res.data === 'string') {
+    alert(res.data);
     throw error;
   }
-  alert(msg);
+  alert('알 수 없는 문제가 발생했습니다.');
   throw error;
 });
