0e98e70d6aa6866a69135dabe23a42600391e1e0
bellatoris <trollbellator@gmail.com>
2022-11-13 03:21:02 +0900

    create iam and s3 resources for s3

--- infra/terraform/scc/iam.tf
+++ infra/terraform/scc/iam.tf
@@ -1,4 +1,5 @@
-data "aws_iam_policy_document" "external_secret_role_policy" {
+# external secret
+data "aws_iam_policy_document" "external_secret" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
@@ -13,9 +14,24 @@ data "aws_iam_policy_document" "external_secret_role_policy" {
       values   = ["system:serviceaccount:dev:scc-server"]
     }
   }
+
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.terraform_remote_state.oidc.outputs.k3s_oidc_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "k3s.staircrusher.club:sub"
+      values   = ["system:serviceaccount:scc:scc-server"]
+    }
+  }
 }
 
-data "aws_iam_policy_document" "read_secret_manager" {
+data "aws_iam_policy_document" "secret_manager_read" {
   statement {
     actions = [
       "secretsmanager:GetResourcePolicy",
@@ -27,17 +43,62 @@ data "aws_iam_policy_document" "read_secret_manager" {
   }
 }
 
-resource "aws_iam_role" "external_secret_role" {
-  name               = "external_secret_role"
-  assume_role_policy = data.aws_iam_policy_document.external_secret_role_policy.json
+resource "aws_iam_role" "external_secret" {
+  name               = "external-secret"
+  assume_role_policy = data.aws_iam_policy_document.external_secret.json
 }
 
-resource "aws_iam_policy" "read_secret_manager" {
-  name   = "read_secret_manager"
-  policy = data.aws_iam_policy_document.read_secret_manager.json
+resource "aws_iam_policy" "secret_manager_read" {
+  name   = "secret_manager_read"
+  policy = data.aws_iam_policy_document.secret_manager_read.json
 }
 
 resource "aws_iam_role_policy_attachment" "external_secret_role_attach" {
-  role       = aws_iam_role.external_secret_role.name
-  policy_arn = aws_iam_policy.read_secret_manager.arn
+  role       = aws_iam_role.external_secret.name
+  policy_arn = aws_iam_policy.secret_manager_read.arn
+}
+
+# scc
+data "aws_iam_policy_document" "scc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.terraform_remote_state.oidc.outputs.k3s_oidc_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "k3s.staircrusher.club:sub"
+      values   = ["system:serviceaccount:scc:scc-server"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "scc_accessibility_images_full_access" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      aws_s3_bucket.accessibility_images.arn,
+      "${aws_s3_bucket.accessibility_images.arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_role" "scc" {
+  name               = "scc"
+  assume_role_policy = data.aws_iam_policy_document.scc.json
+}
+
+resource "aws_iam_policy" "scc_accessibility_images_full_access" {
+  name   = "scc-accsseibility-images-pull-access"
+  policy = data.aws_iam_policy_document.scc_accessibility_images_full_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "scc_accessibility_images_full_access" {
+  role       = aws_iam_role.scc.name
+  policy_arn = aws_iam_policy.scc_accessibility_images_full_access.arn
 }
---
+++ infra/terraform/scc/s3.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "dev_accessibility_images" {
+  bucket = "scc-dev-accessibility-images"
+}
+
+resource "aws_s3_bucket" "accessibility_images" {
+  bucket = "scc-accrssibility-images"
+}
