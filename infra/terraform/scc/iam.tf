5425ce378581f79e2e2346ac7e1678949fd8c6cc
Zeniuus <jsh56son@gmail.com>
2022-11-19 13:47:52 +0900

    실섭 secret 배포를 위한 리소스 생성

--- infra/terraform/scc/iam.tf
+++ infra/terraform/scc/iam.tf
@@ -42,3 +42,50 @@ resource "aws_iam_role_policy_attachment" "scc_accessibility_images_full_access"
   role       = aws_iam_role.scc.name
   policy_arn = aws_iam_policy.scc_accessibility_images_full_access.arn
 }
+
+data "aws_iam_policy_document" "scc_deploy_secret" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.terraform_remote_state.oidc.outputs.k3s_oidc_arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "k3s.staircrusher.club:sub"
+      values   = ["system:serviceaccount:scc:scc-server-deploy-secret"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "scc_deploy_secret_kms_access" {
+  statement {
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+    resources = [
+      data.terraform_remote_state.kms.outputs.sops_kms_key_arn
+    ]
+  }
+}
+
+resource "aws_iam_role" "scc_deploy_secret" {
+  name               = "scc-deploy-secret"
+  assume_role_policy = data.aws_iam_policy_document.scc_deploy_secret.json
+}
+
+resource "aws_iam_policy" "scc_deploy_secret_kms_access" {
+  name   = "scc-deploy-secret-kms-access"
+  policy = data.aws_iam_policy_document.scc_deploy_secret_kms_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "scc_deploy_secret_kms_read_access" {
+  role       = aws_iam_role.scc_deploy_secret.name
+  policy_arn = aws_iam_policy.scc_deploy_secret_kms_access.arn
+}
