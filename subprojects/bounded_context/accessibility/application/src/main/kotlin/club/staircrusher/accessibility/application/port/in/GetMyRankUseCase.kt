0305c53cdd90d6fe1a221f921155d7ccf14d652a
bellatoris <trollbellator@gmail.com>
2023-04-23 22:03:50 +0900

    implement rank use cases

--- subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/GetMyRankUseCase.kt
+++ subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/GetMyRankUseCase.kt
@@ -1,11 +1,15 @@
 package club.staircrusher.accessibility.application.port.`in`
 
+import club.staircrusher.accessibility.application.port.out.persistence.AccessibilityRankRepository
 import club.staircrusher.accessibility.domain.model.AccessibilityRank
 import club.staircrusher.stdlib.di.annotation.Component
+import club.staircrusher.stdlib.domain.SccDomainException
 
 @Component
-class GetMyRankUseCase {
+class GetMyRankUseCase(
+    private val accessibilityRankRepository: AccessibilityRankRepository,
+) {
     fun handle(userId: String): AccessibilityRank {
-        TODO()
+        return accessibilityRankRepository.findByUserId(userId) ?: throw SccDomainException("잘못된 계정입니다.")
     }
 }
