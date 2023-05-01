871965ff367ff9a102149bbe91c295a0570dcec1
bellatoris <trollbellator@gmail.com>
2023-05-01 18:44:12 +0900

    refine terraform

--- infra/terraform/k3s/domain/thumbprint.sh
+++ infra/terraform/k3s/domain/thumbprint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -xe
+
 openssl s_client -showcerts -verify 5 -connect $1:443 < /dev/null |
   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".pem"; print >out}'
 
 for cert in *.pem; do
-  THUMBPRINT=$(openssl x509 -fingerprint -noout -in $cert | sed 's/://g' | awk -F= '{print tolower($2)}')
+  THUMBPRINT=$(openssl x509 -fingerprint -noout -sha1 -in $cert | sed 's/://g' | awk -F= '{print tolower($2)}')
   THUMBPRINT_JSON="{\"thumbprint\": \"${THUMBPRINT}\"}"
 done
 
--- infra/terraform/k3s/lightsail.tf
+++ infra/terraform/k3s/lightsail.tf
@@ -86,67 +86,10 @@ curl -sfL https://get.k3s.io | sh -s - agent \
 USERDATA
 }
 
-resource "aws_lightsail_instance" "k3s_data_plane" {
-  name              = "k3s_data_plane_1"
-  availability_zone = "ap-northeast-2c"
-  key_pair_name     = aws_lightsail_key_pair.scc_key_pair.name
-  bundle_id         = "small_2_0"
-  blueprint_id      = "ubuntu_20_04"
-  user_data         = local.data_plane_userdata
-  tags = {
-    role = "data_plane"
-  }
-}
-
-resource "aws_lightsail_instance_public_ports" "k3s_data_plane" {
-  instance_name = aws_lightsail_instance.k3s_data_plane.name
-
-  port_info {
-    protocol  = "tcp"
-    from_port = 22
-    to_port   = 22
-    cidrs     = ["172.26.0.0/20"]
-  }
-
-  port_info {
-    protocol  = "tcp"
-    from_port = var.node_port
-    to_port   = var.node_port
-    cidrs     = ["0.0.0.0/0"]
-  }
-
-  port_info {
-    protocol  = "tcp"
-    from_port = 443
-    to_port   = 443
-    cidrs     = ["172.26.0.0/20"]
-  }
-
-  port_info {
-    protocol  = "tcp"
-    from_port = 10250
-    to_port   = 10250
-    cidrs     = ["172.26.0.0/20"]
-  }
-
-  port_info {
-    protocol  = "udp"
-    from_port = 8472
-    to_port   = 8472
-    cidrs     = ["172.26.0.0/20"]
-  }
-
-  lifecycle {
-    replace_triggered_by = [
-      aws_lightsail_instance.k3s_data_plane.id
-    ]
-  }
-}
-
 resource "aws_lightsail_instance" "k3s_data_planes" {
-  for_each = { for i in range(1): i => i }
+  for_each = { for i in range(2) : i => i }
 
-  name              = "k3s_data_plane_${each.value + 2}"
+  name              = "k3s_data_plane_${each.value}"
   availability_zone = element(["ap-northeast-2a", "ap-northeast-2c"], each.value % 2)
   key_pair_name     = aws_lightsail_key_pair.scc_key_pair.name
   bundle_id         = "small_2_0"
