bdc0429e95c37677f72fd51a196ea7e07406d9a7
bellatoris <trollbellator@gmail.com>
2023-08-01 00:27:45 +0900

    setup scc related resources

--- infra/terraform/scc/s3.tf
+++ infra/terraform/scc/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "dev_accessibility_images" {
-  bucket = "scc-dev-accessibility-images"
+  bucket = "scc-dev-accessibility-images-2"
 }
 
 resource "aws_s3_bucket" "accessibility_images" {
-  bucket = "scc-accessibility-images"
+  bucket = "scc-prod-accessibility-images"
 }
