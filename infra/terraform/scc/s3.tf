42f9670baed8be1283b4755f435301bf60529fed
Suhwan Jee <jsh56son@gmail.com>
2023-08-15 16:13:45 +0900

    [skip ci] S3 presigned url을 위한 S3 bucket 접근 권한 설정 (#184)

--- infra/terraform/scc/s3.tf
+++ infra/terraform/scc/s3.tf
@@ -2,6 +2,40 @@ resource "aws_s3_bucket" "dev_accessibility_images" {
   bucket = "scc-dev-accessibility-images-2"
 }
 
+resource "aws_s3_bucket_ownership_controls" "dev_accessibility_images" {
+  bucket = aws_s3_bucket.dev_accessibility_images.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "dev_accessibility_images" {
+  bucket = aws_s3_bucket.dev_accessibility_images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket" "accessibility_images" {
   bucket = "scc-prod-accessibility-images"
+}
+
+resource "aws_s3_bucket_ownership_controls" "accessibility_images" {
+  bucket = aws_s3_bucket.accessibility_images.id
+
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "accessibility_images" {
+  bucket = aws_s3_bucket.accessibility_images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
