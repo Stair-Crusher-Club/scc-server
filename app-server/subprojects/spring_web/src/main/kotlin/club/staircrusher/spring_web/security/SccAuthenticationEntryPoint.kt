1fc3d137eeb3df66a12a0f5c5f99175f8b8a852c
nelson kim <yongseongkimm@gmail.com>
2023-08-27 16:12:23 +0900

    유효하지 않은 로그인 정보 토스트로 알려주기 (#192)

--- app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
+++ app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
@@ -1,5 +1,6 @@
 package club.staircrusher.spring_web.security
 
+import club.staircrusher.user.domain.exception.UserAuthenticationException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
@@ -14,6 +15,13 @@ class SccAuthenticationEntryPoint : AuthenticationEntryPoint {
         response: HttpServletResponse,
         authException: AuthenticationException?
     ) {
-        response.sendError(HttpStatus.UNAUTHORIZED.value())
+        when (authException?.cause) {
+            is UserAuthenticationException -> {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 유효하지 않습니다. 다시 로그인해주세요.")
+            }
+            else -> {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), authException?.message ?: "")
+            }
+        }
     }
 }
