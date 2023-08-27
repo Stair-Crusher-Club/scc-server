d29493d3ec4c92e7ee4fccfadf61b37dd0c581c3
yongseongkim <yongseongkimm@gmail.com>
2023-08-28 00:05:40 +0900

    인증 실패시에 ApiErrorResponse 내려주기

--- app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
+++ app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
@@ -1,5 +1,7 @@
 package club.staircrusher.spring_web.security
 
+import club.staircrusher.admin_api.spec.infrastructure.Serializer
+import club.staircrusher.api.spec.dto.ApiErrorResponse
 import club.staircrusher.user.domain.exception.UserAuthenticationException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -15,13 +17,19 @@ class SccAuthenticationEntryPoint : AuthenticationEntryPoint {
         response: HttpServletResponse,
         authException: AuthenticationException?
     ) {
-        when (authException?.cause) {
-            is UserAuthenticationException -> {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 유효하지 않습니다. 다시 로그인해주세요.")
-            }
-            else -> {
-                response.sendError(HttpStatus.UNAUTHORIZED.value(), authException?.message ?: "")
-            }
-        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.writer.write(
+            Serializer.jacksonObjectMapper.writeValueAsString(
+                ApiErrorResponse(
+                    msg = when (authException?.cause) {
+                        is UserAuthenticationException -> "로그인 정보가 유효하지 않습니다. 다시 로그인해주세요."
+                        else -> authException?.message ?: ""
+                    },
+                    code = ApiErrorResponse.Code.UNAUTHORIZED
+                )
+            )
+        )
     }
 }
