f681f56485991d61074850c85f39745f94d57f76
yongseongkim <yongseongkimm@gmail.com>
2023-08-28 00:20:08 +0900

    Defined objectmapper

--- app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
+++ app-server/subprojects/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccAuthenticationEntryPoint.kt
@@ -1,8 +1,8 @@
 package club.staircrusher.spring_web.security
 
-import club.staircrusher.admin_api.spec.infrastructure.Serializer
 import club.staircrusher.api.spec.dto.ApiErrorResponse
 import club.staircrusher.user.domain.exception.UserAuthenticationException
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpStatus
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component
 
 @Component
 class SccAuthenticationEntryPoint : AuthenticationEntryPoint {
+    private val objectMapper = jacksonObjectMapper()
+
     override fun commence(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -21,7 +23,7 @@ class SccAuthenticationEntryPoint : AuthenticationEntryPoint {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.writer.write(
-            Serializer.jacksonObjectMapper.writeValueAsString(
+            objectMapper.writeValueAsString(
                 ApiErrorResponse(
                     msg = when (authException?.cause) {
                         is UserAuthenticationException -> "로그인 정보가 유효하지 않습니다. 다시 로그인해주세요."
