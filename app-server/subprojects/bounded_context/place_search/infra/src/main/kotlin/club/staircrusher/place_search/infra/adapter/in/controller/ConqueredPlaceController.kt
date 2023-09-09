fcc80e551dfd8797390d22382183395d148b5a15
yongseongkim <yongseongkimm@gmail.com>
2023-09-09 15:57:20 +0900

    안쓰는 ListConqueredPlacesRequest 제거하기

--- app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ConqueredPlaceController.kt
+++ app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ConqueredPlaceController.kt
@@ -1,11 +1,9 @@
 package club.staircrusher.place_search.infra.adapter.`in`.controller
 
-import club.staircrusher.api.spec.dto.ListConqueredPlacesRequestDto
 import club.staircrusher.api.spec.dto.ListConqueredPlacesResponseDto
 import club.staircrusher.place_search.application.port.`in`.ListConqueredPlacesQuery
 import club.staircrusher.spring_web.security.app.SccAppAuthentication
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,13 +12,12 @@ class ConqueredPlaceController(
 ) {
     @PostMapping("/listConqueredPlaces")
     fun listConqueredPlaces(
-        authentication: SccAppAuthentication,
-        @RequestBody request: ListConqueredPlacesRequestDto,
+        authentication: SccAppAuthentication
     ): ListConqueredPlacesResponseDto {
         val items = listConqueredPlacesQuery.listConqueredPlaces(authentication.principal)
         // 페이징 로직은 필요할 때 대응한다.
         return ListConqueredPlacesResponseDto(
-            totalNumberOfItems = items.count(),
+            totalNumberOfItems = items.count().toLong(),
             nextToken = null,
             items = items.map { it.toDTO() }
         )
