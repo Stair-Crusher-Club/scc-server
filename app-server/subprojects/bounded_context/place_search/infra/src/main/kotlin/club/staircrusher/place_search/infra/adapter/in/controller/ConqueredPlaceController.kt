cbeb9e914d0c40ddf2b1a488d82fabf4be642bb7
yongseongkim <yongseongkimm@gmail.com>
2023-09-09 12:39:18 +0900

    next token, limit 추가하지만 쓰지 않음

--- app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ConqueredPlaceController.kt
+++ app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ConqueredPlaceController.kt
@@ -1,9 +1,11 @@
 package club.staircrusher.place_search.infra.adapter.`in`.controller
 
+import club.staircrusher.api.spec.dto.ListConqueredPlacesRequestDto
 import club.staircrusher.api.spec.dto.ListConqueredPlacesResponseDto
 import club.staircrusher.place_search.application.port.`in`.ListConqueredPlacesQuery
 import club.staircrusher.spring_web.security.app.SccAppAuthentication
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,10 +13,15 @@ class ConqueredPlaceController(
     private val listConqueredPlacesQuery: ListConqueredPlacesQuery,
 ) {
     @PostMapping("/listConqueredPlaces")
-    fun listConqueredPlaces(authentication: SccAppAuthentication): ListConqueredPlacesResponseDto {
+    fun listConqueredPlaces(
+        authentication: SccAppAuthentication,
+        @RequestBody request: ListConqueredPlacesRequestDto,
+    ): ListConqueredPlacesResponseDto {
         val items = listConqueredPlacesQuery.listConqueredPlaces(authentication.principal)
+        // 페이징 로직은 필요할 때 대응한다.
         return ListConqueredPlacesResponseDto(
             totalNumberOfItems = items.count(),
+            nextToken = null,
             items = items.map { it.toDTO() }
         )
     }
