16ce85becaa890c76332735cee2d893df9338604
yongseongkim <yongseongkimm@gmail.com>
2023-09-18 23:49:17 +0900

    Simplify equals and hasCode

--- app-server/subprojects/bounded_context/challenge/domain/src/main/kotlin/club/staircrusher/challenge/domain/model/ChallengeContribution.kt
+++ app-server/subprojects/bounded_context/challenge/domain/src/main/kotlin/club/staircrusher/challenge/domain/model/ChallengeContribution.kt
@@ -18,28 +18,10 @@ class ChallengeContribution(
         if (javaClass != other?.javaClass) return false
 
         other as ChallengeContribution
-
-        if (id != other.id) return false
-        if (userId != other.userId) return false
-        if (challengeId != other.challengeId) return false
-        if (placeAccessibilityId != other.placeAccessibilityId) return false
-        if (placeAccessibilityCommentId != other.placeAccessibilityCommentId) return false
-        if (buildingAccessibilityId != other.buildingAccessibilityId) return false
-        if (buildingAccessibilityCommentId != other.buildingAccessibilityCommentId) return false
-        if (createdAt != other.createdAt) return false
-        return updatedAt == other.updatedAt
+        return id == other.id
     }
 
     override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + userId.hashCode()
-        result = 31 * result + challengeId.hashCode()
-        result = 31 * result + (placeAccessibilityId?.hashCode() ?: 0)
-        result = 31 * result + (placeAccessibilityCommentId?.hashCode() ?: 0)
-        result = 31 * result + (buildingAccessibilityId?.hashCode() ?: 0)
-        result = 31 * result + (buildingAccessibilityCommentId?.hashCode() ?: 0)
-        result = 31 * result + createdAt.hashCode()
-        result = 31 * result + updatedAt.hashCode()
-        return result
+        return id.hashCode()
     }
 }
