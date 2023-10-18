9794e2fe689c31ad3739d91529cc3d9bfacf1128
bellatoris <trollbellator@gmail.com>
2023-10-18 20:58:59 +0900

    leverage the existing method

--- app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetCountForNextChallengeRankUseCase.kt
+++ app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetCountForNextChallengeRankUseCase.kt
@@ -1,6 +1,6 @@
 package club.staircrusher.challenge.application.port.`in`.use_case
 
-import club.staircrusher.challenge.application.port.out.persistence.ChallengeParticipationRepository
+import club.staircrusher.challenge.application.port.`in`.ChallengeService
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeRankRepository
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeRepository
 import club.staircrusher.stdlib.di.annotation.Component
@@ -18,12 +18,12 @@ class GetCountForNextChallengeRankUseCase(
     private val userApplicationService: UserApplicationService,
     private val challengeRepository: ChallengeRepository,
     private val challengeRankRepository: ChallengeRankRepository,
-    private val challengeParticipationRepository: ChallengeParticipationRepository,
+    private val challengeService: ChallengeService,
 ) {
     fun handle(challengeId: String, userId: String): Int  = transactionManager.doInTransaction {
         userApplicationService.getUser(userId) ?: throw SccDomainException("잘못된 계정입니다.")
         val challenge = challengeRepository.findByIdOrNull(challengeId) ?: throw SccDomainException("잘못된 챌린지입니다.")
-        if (challengeParticipationRepository.findByChallengeIdAndUserId(challengeId, userId) == null) {
+        if (!challengeService.hasJoined(userId, challengeId)) {
             throw SccDomainException("참여하지 않은 챌린지 입니다.")
         }
 
