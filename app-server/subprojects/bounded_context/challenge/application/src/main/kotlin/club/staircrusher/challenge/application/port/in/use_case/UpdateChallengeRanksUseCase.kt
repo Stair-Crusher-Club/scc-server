4a7c6b8437c7a68ef03a6a9dc6e14f37cbaffa82
bellatoris <trollbellator@gmail.com>
2023-10-18 20:50:19 +0900

    split transacion boundary

--- app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/UpdateChallengeRanksUseCase.kt
+++ app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/UpdateChallengeRanksUseCase.kt
@@ -7,6 +7,7 @@ import club.staircrusher.challenge.domain.model.ChallengeRank
 import club.staircrusher.stdlib.clock.SccClock
 import club.staircrusher.stdlib.di.annotation.Component
 import club.staircrusher.stdlib.domain.entity.EntityIdGenerator
+import club.staircrusher.stdlib.persistence.TransactionIsolationLevel
 import club.staircrusher.stdlib.persistence.TransactionManager
 
 /**
@@ -19,41 +20,43 @@ class UpdateChallengeRanksUseCase(
     private val challengeRankRepository: ChallengeRankRepository,
     private val challengeContributionRepository: ChallengeContributionRepository,
 ) {
-    fun handle() = transactionManager.doInTransaction {
+    fun handle() {
         val challenges = challengeRepository.findAllOrderByCreatedAtDesc()
         challenges.forEach { challenge ->
             val now = SccClock.instant()
-            val lastRank = challengeRankRepository.findByContributionCount(challenge.id, 0)?.rank ?: 1
-            val contributions = challengeContributionRepository.findByChallengeId(challenge.id)
+            transactionManager.doInTransaction(TransactionIsolationLevel.SERIALIZABLE) {
+                val contributions = challengeContributionRepository.findByChallengeId(challenge.id)
 
-            val ranks = contributions.groupBy { it.userId }
-                .map { (userId, contributions) ->
-                    val contributionCount = contributions.size
-                    val challengeRank = challengeRankRepository.findByUserId(challenge.id, userId) ?: ChallengeRank(
-                        id = EntityIdGenerator.generateRandom(),
-                        challengeId = challenge.id,
-                        userId = userId,
-                        contributionCount = contributionCount,
-                        rank = lastRank,
-                        createdAt = now,
-                        updatedAt = now,
-                    )
+                val ranks = contributions.groupBy { it.userId }
+                    .map { (userId, contributions) ->
+                        val contributionCount = contributions.size
+                        val challengeRank = challengeRankRepository.findByUserId(challenge.id, userId) ?: ChallengeRank(
+                            id = EntityIdGenerator.generateRandom(),
+                            challengeId = challenge.id,
+                            userId = userId,
+                            contributionCount = contributionCount,
+                            rank = -1,
+                            createdAt = now,
+                            updatedAt = now,
+                        )
 
-                    challengeRank.copy(
-                        contributionCount = contributionCount,
-                        updatedAt = now,
-                    )
-                }
+                        challengeRank.copy(
+                            contributionCount = contributionCount,
+                            updatedAt = now,
+                        )
+                    }
 
-            var nextRank = 1L
-            val updatedRanks = ranks.groupBy { it.contributionCount }
-                .toSortedMap(compareByDescending { it })
-                .flatMap { (_, ranks) ->
-                    val currentRank = nextRank
-                    nextRank += ranks.size
-                    ranks.map { it.copy(rank = currentRank, updatedAt = now) }
-                }
-            challengeRankRepository.saveAll(updatedRanks)
+                var nextRank = 1L
+                val updatedRanks = ranks.groupBy { it.contributionCount }
+                    .toSortedMap(compareByDescending { it })
+                    .flatMap { (_, ranks) ->
+                        val currentRank = nextRank
+                        nextRank += ranks.size
+                        ranks.map { it.copy(rank = currentRank, updatedAt = now) }
+                    }
+                challengeRankRepository.removeAll()
+                challengeRankRepository.saveAll(updatedRanks)
+            }
         }
     }
 }
