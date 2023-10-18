9dd20c4cea48f4ef1b40d0dfb8ce1379cec15906
Zeniuus <jsh56son@gmail.com>
2023-10-18 18:15:29 +0900

    Fix: 임시로 어드민 의존성 깨지는 것 해결

--- app-admin-frontend/docker-push.sh
+++ app-admin-frontend/docker-push.sh
@@ -12,7 +12,7 @@ fi;
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/q0g6g7m8
 
-npm install
+npm install -f # FIXME: 현재 사용하고 있는 blueprintjs 버전이 react 17 버전에 의존하는데, 실제로 사용하는 버전은 18이라서 -f가 없으면 오류가 난다.
 npm run "build:$1"
 docker buildx build --platform linux/amd64 -t public.ecr.aws/i6n1n6v2/scc-admin-frontend:$IMAGE_TAG .
 docker push public.ecr.aws/i6n1n6v2/scc-admin-frontend:$IMAGE_TAG
