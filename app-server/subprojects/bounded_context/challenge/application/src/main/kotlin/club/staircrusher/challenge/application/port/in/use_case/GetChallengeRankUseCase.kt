278121dd256bacfd5c4dd1b3005a15125fa0ac09
bellatoris <trollbellator@gmail.com>
2023-10-29 17:00:50 +0900

    Update logic to return null for users who recently joined

--- app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetChallengeRankUseCase.kt
+++ app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetChallengeRankUseCase.kt
@@ -4,12 +4,10 @@ import club.staircrusher.challenge.application.port.`in`.ChallengeService
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeRankRepository
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeRepository
 import club.staircrusher.challenge.domain.model.ChallengeRank
-import club.staircrusher.stdlib.clock.SccClock
 import club.staircrusher.stdlib.di.annotation.Component
 import club.staircrusher.stdlib.domain.SccDomainException
 import club.staircrusher.stdlib.persistence.TransactionManager
 import club.staircrusher.user.application.port.`in`.UserApplicationService
-import java.util.UUID
 
 @Component
 class GetChallengeRankUseCase(
@@ -19,28 +17,15 @@ class GetChallengeRankUseCase(
     private val challengeRankRepository: ChallengeRankRepository,
     private val challengeService: ChallengeService,
 ) {
-    fun handle(challengeId: String, userId: String): ChallengeRank = transactionManager.doInTransaction {
-        val now = SccClock.instant()
-        val user = userApplicationService.getUser(userId) ?: throw SccDomainException("잘못된 계정입니다.")
+    fun handle(challengeId: String, userId: String): ChallengeRank? = transactionManager.doInTransaction {
+        userApplicationService.getUser(userId) ?: throw SccDomainException("잘못된 계정입니다.")
+
         val challenge = challengeRepository.findByIdOrNull(challengeId) ?: throw SccDomainException("잘못된 챌린지입니다.")
         if (!challengeService.hasJoined(userId, challengeId)) {
             throw SccDomainException("참여하지 않은 챌린지 입니다.")
         }
 
-        val challengeRank = challengeRankRepository.findByUserId(challenge.id, userId) ?: run {
-            // if lastRank can not be found, then the user is the first rank
-            val lastRank = challengeRankRepository.findLastRank(challengeId) ?: 1
-            ChallengeRank(
-                id = UUID.randomUUID().toString(),
-                challengeId = challenge.id,
-                userId = user.id,
-                contributionCount = 0,
-                rank = lastRank,
-                createdAt = now,
-                updatedAt = now,
-            )
-        }
-
-        challengeRank
+        // if the user does not have rank yet, return null and let the user know that the rank will be updated soon
+        challengeRankRepository.findByUserId(challenge.id, userId)
     }
 }
--- app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetCountForNextChallengeRankUseCase.kt
+++ app-server/subprojects/bounded_context/challenge/application/src/main/kotlin/club/staircrusher/challenge/application/port/in/use_case/GetCountForNextChallengeRankUseCase.kt
@@ -20,21 +20,22 @@ class GetCountForNextChallengeRankUseCase(
     private val challengeRankRepository: ChallengeRankRepository,
     private val challengeService: ChallengeService,
 ) {
-    fun handle(challengeId: String, userId: String): Int  = transactionManager.doInTransaction {
+    fun handle(challengeId: String, userId: String): Int?  = transactionManager.doInTransaction {
         userApplicationService.getUser(userId) ?: throw SccDomainException("잘못된 계정입니다.")
         val challenge = challengeRepository.findByIdOrNull(challengeId) ?: throw SccDomainException("잘못된 챌린지입니다.")
         if (!challengeService.hasJoined(userId, challengeId)) {
             throw SccDomainException("참여하지 않은 챌린지 입니다.")
         }
 
-        val currentRank = challengeRankRepository.findByUserId(challenge.id, userId)
-        val rank = currentRank?.rank ?: challengeRankRepository.findLastRank(challenge.id) ?: 1
+        // if the user does not have rank yet, return null and let the user know that the rank will be updated soon
+        val challengeRank = challengeRankRepository.findByUserId(challenge.id, userId)
+        val rank =  challengeRank?.rank ?: return@doInTransaction null
 
         if (rank == 1L) {
             0
         } else {
             val nextRank = challengeRankRepository.findNextRank(challenge.id, rank)!!
-            nextRank.contributionCount - (currentRank?.contributionCount ?: 0)
+            nextRank.contributionCount - challengeRank.contributionCount
         }
     }
 }
