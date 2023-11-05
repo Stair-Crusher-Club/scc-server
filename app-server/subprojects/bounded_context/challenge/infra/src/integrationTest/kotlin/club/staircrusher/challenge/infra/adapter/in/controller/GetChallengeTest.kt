7cc278e6d08d9cb966a3880e1eb80ad71fe58cc8
bellatoris <trollbellator@gmail.com>
2023-11-05 21:52:20 +0900

    comment in the rank tests

--- app-server/subprojects/bounded_context/challenge/infra/src/integrationTest/kotlin/club/staircrusher/challenge/infra/adapter/in/controller/GetChallengeTest.kt
+++ app-server/subprojects/bounded_context/challenge/infra/src/integrationTest/kotlin/club/staircrusher/challenge/infra/adapter/in/controller/GetChallengeTest.kt
@@ -6,7 +6,12 @@ import club.staircrusher.api.spec.dto.GetChallengeWithInvitationCodeRequestDto
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeContributionRepository
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeParticipationRepository
 import club.staircrusher.challenge.application.port.out.persistence.ChallengeRepository
+import club.staircrusher.challenge.domain.model.Challenge
+import club.staircrusher.challenge.domain.model.ChallengeContribution
 import club.staircrusher.challenge.infra.adapter.`in`.controller.base.ChallengeITBase
+import club.staircrusher.user.domain.model.User
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -47,27 +52,7 @@ class GetChallengeTest : ChallengeITBase() {
         assertTrue(challengeBeforeParticipationAndContribution.participationsCount == 0)
         assertTrue(challengeBeforeParticipationAndContribution.contributionsCount == 0)
 
-        val users = transactionManager.doInTransaction {
-            (0 until Random.nextLong(from = 1, until = 10))
-                .map {
-                    val user = testDataGenerator.createUser()
-                    participate(user, challenge)
-                    return@map user
-                }
-        }
-        val contributions = (0 until Random.nextLong(from = 10, until = 100))
-            .map {
-                contributePlaceAccessibility(
-                    user = users.random(),
-                    challenge = challenge
-                )
-            }
-
-        mvc.sccRequest(
-            "/updateChallengeRanks",
-            null,
-        )
-            .andReturn()
+        val (users, contributions) = generateUsersAndContributions(challenge)
 
         val getChallengeResponse = mvc.sccRequest(
             "/getChallenge",
@@ -77,11 +62,44 @@ class GetChallengeTest : ChallengeITBase() {
             user = users.first()
         )
             .getResult(GetChallengeResponseDto::class)
+
         // 참여 후 상태 확인
         assertTrue(getChallengeResponse.challenge.id == challenge.id)
+        assertNotNull(getChallengeResponse.myRank)
         assertTrue(getChallengeResponse.challenge.participationsCount == users.count())
         assertTrue(getChallengeResponse.challenge.contributionsCount == contributions.count())
         assertTrue(getChallengeResponse.hasJoined)
+    }
+
+    private fun generateUsersAndContributions(
+        challenge: Challenge,
+        participant: User? = null,
+    ): Pair<List<User>, List<ChallengeContribution>> {
+        val users = mutableListOf<User>()
+        transactionManager.doInTransaction {
+            repeat(Random.nextInt(from = 1, until = 10)) {
+                users += testDataGenerator.createUser().also { participate(it, challenge) }
+            }
+        }
+        users += listOfNotNull(participant?.also { participate(it, challenge) })
+
+        val contributions = mutableListOf<ChallengeContribution>()
+        users.forEach { user ->
+            repeat(Random.nextInt(from = 10, until = 100)) {
+                contributions += contributePlaceAccessibility(
+                    user = user,
+                    challenge = challenge,
+                )
+            }
+        }
+
+        mvc.sccRequest(
+            "/updateChallengeRanks",
+            null,
+        )
+            .andReturn()
+
+        return Pair(users, contributions)
     }
 
     @Test
@@ -144,65 +162,41 @@ class GetChallengeTest : ChallengeITBase() {
         assertTrue(challengeAfterComplete.isComplete)
     }
 
-    // TODO: 랭킹 구현 후 테스트 구현
+   @Test
+   fun `진행 중인 챌린지에 참여하지 않았다면 다른 사람들의 랭킹이 보이지 않는다`() {
+       val inProgressChallenge = registerInProgressChallenge()
+       val user = testDataGenerator.createUser()
 
-//    @Test
-//    fun `진행 중인 챌린지에 참여하지 않았다면 다른 사람들의 랭킹이 보이지 않는다`() {
-//        val inProgressChallenge = registerInProgressChallenge()
-//        val user = testDataGenerator.createUser()
-//
-//        val getChallengeResponseDto = mvc.sccRequest(
-//            "/getChallenge",
-//            GetChallengeRequestDto(
-//                challengeId = inProgressChallenge.id
-//            ),
-//            user = user
-//        )
-//            .getResult(GetChallengeResponseDto::class)
-//    }
+       val (_, _) = generateUsersAndContributions(inProgressChallenge)
+       val getChallengeResponseDto = mvc.sccRequest(
+           "/getChallenge",
+           GetChallengeRequestDto(
+               challengeId = inProgressChallenge.id
+           ),
+           user = user
+       )
+           .getResult(GetChallengeResponseDto::class)
 
-//    @Test
-//    fun `진행 중인 챌린지에 참여 중이면 내 랭킹과 다른 사람들의 랭킹까지 보여준다`() {
-//        val inProgressChallenge = registerInProgressChallenge()
-//        val user = testDataGenerator.createUser()
-//
-//        val getChallengeResponseDto = mvc.sccRequest(
-//            "/getChallenge",
-//            GetChallengeRequestDto(
-//                challengeId = inProgressChallenge.id
-//            ),
-//            user = user
-//        )
-//            .getResult(GetChallengeResponseDto::class)
-//    }
+       assertTrue(getChallengeResponseDto.ranks.isEmpty())
+       assertNull(getChallengeResponseDto.myRank)
+   }
 
-//    @Test
-//    fun `종료된 챌린지에 참여하지 않았다면 다른 사람들의 랭킹만 보여준다`() {
-//        val closedChallenge = registerClosedChallenge()
-//        val user = testDataGenerator.createUser()
-//
-//        val getChallengeResponseDto = mvc.sccRequest(
-//            "/getChallenge",
-//            GetChallengeRequestDto(
-//                challengeId = closedChallenge.id
-//            ),
-//            user = user
-//        )
-//            .getResult(GetChallengeResponseDto::class)
-//    }
+   @Test
+   fun `진행 중인 챌린지에 참여 중이면 내 랭킹과 다른 사람들의 랭킹까지 보여준다`() {
+       val inProgressChallenge = registerInProgressChallenge()
+       val user = testDataGenerator.createUser()
 
-//    @Test
-//    fun `종료된 챌린지에 이미 참여했었다면 내 랭킹과 다른 사람들의 랭킹을 보여준다`() {
-//        val closedChallenge = registerClosedChallenge()
-//        val user = testDataGenerator.createUser()
-//
-//        val getChallengeResponseDto = mvc.sccRequest(
-//            "/getChallenge",
-//            GetChallengeRequestDto(
-//                challengeId = closedChallenge.id
-//            ),
-//            user = user
-//        )
-//            .getResult(GetChallengeResponseDto::class)
-//    }
+       val (_, _) = generateUsersAndContributions(inProgressChallenge, user)
+       val getChallengeResponseDto = mvc.sccRequest(
+           "/getChallenge",
+           GetChallengeRequestDto(
+               challengeId = inProgressChallenge.id
+           ),
+           user = user
+       )
+           .getResult(GetChallengeResponseDto::class)
+
+       assertTrue(getChallengeResponseDto.ranks.isNotEmpty())
+       assertNotNull(getChallengeResponseDto.myRank)
+   }
 }
