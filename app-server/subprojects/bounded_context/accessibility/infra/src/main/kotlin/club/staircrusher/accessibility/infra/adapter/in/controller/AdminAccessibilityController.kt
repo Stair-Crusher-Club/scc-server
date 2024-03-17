7a59d3ea1300a625fe606e83f8c5c1f0e21527c8
Zeniuus <jsh56son@gmail.com>
2024-03-17 15:08:47 +0900

    어드민 장소 & 건물 정보 삭제 API 작성

--- app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AdminAccessibilityController.kt
+++ app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AdminAccessibilityController.kt
@@ -1,8 +1,12 @@
 package club.staircrusher.accessibility.infra.adapter.`in`.controller
 
+import club.staircrusher.accessibility.application.port.`in`.AdminDeleteBuildingAccessibilityUseCase
+import club.staircrusher.accessibility.application.port.`in`.AdminDeletePlaceAccessibilityUseCase
 import club.staircrusher.accessibility.application.port.`in`.AdminSearchAccessibilitiesUseCase
 import club.staircrusher.admin_api.spec.dto.AdminAccessibilityDTO
 import club.staircrusher.admin_api.spec.dto.AdminSearchAccessibilitiesResultDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -10,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class AdminAccessibilityController(
     private val adminSearchAccessibilitiesUseCase: AdminSearchAccessibilitiesUseCase,
+    private val adminDeletePlaceAccessibilityUseCase: AdminDeletePlaceAccessibilityUseCase,
+    private val adminDeleteBuildingAccessibilityUseCase: AdminDeleteBuildingAccessibilityUseCase,
 ) {
     @GetMapping("/admin/accessibilities/search")
     fun searchAccessibilities(
-        @RequestParam placeName: String?,
-        @RequestParam cursor: String?,
-        @RequestParam limit: Int?,
+        @RequestParam(required = false) placeName: String?,
+        @RequestParam(required = false) cursor: String?,
+        @RequestParam(required = false) limit: Int?,
     ): AdminSearchAccessibilitiesResultDTO {
         val result = adminSearchAccessibilitiesUseCase.handle(
             placeName = placeName,
@@ -38,5 +44,25 @@ class AdminAccessibilityController(
             },
             cursor = result.cursor,
         )
+    }
+
+    @DeleteMapping("/admin/place-accessibilities/{id}")
+    fun deletePlaceAccessibility(
+        @RequestParam id: String,
+    ): ResponseEntity<Unit> {
+        adminDeletePlaceAccessibilityUseCase.handle(placeAccessibilityId = id)
+        return ResponseEntity
+            .noContent()
+            .build()
+    }
+
+    @DeleteMapping("/admin/building-accessibilities/{id}")
+    fun deleteBuildingAccessibility(
+        @RequestParam id: String,
+    ): ResponseEntity<Unit> {
+        adminDeleteBuildingAccessibilityUseCase.handle(buildingAccessibilityId = id)
+        return ResponseEntity
+            .noContent()
+            .build()
     }
 }
