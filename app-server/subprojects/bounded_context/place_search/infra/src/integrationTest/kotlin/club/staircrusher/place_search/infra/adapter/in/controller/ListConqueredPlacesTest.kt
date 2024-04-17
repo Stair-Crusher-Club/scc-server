a28a521d3f6638d3a3a1b93f7fd0f879a588d9c5
sanggggg <37951125+sanggggg@users.noreply.github.com>
2024-04-18 01:29:36 +0900

    내가 정복한 장소는 장소 정복 등록 시간 역순으로 내려준다 (#264)
    * sort conquered
    
    * add test

--- app-server/subprojects/bounded_context/place_search/infra/src/integrationTest/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ListConqueredPlacesTest.kt
+++ app-server/subprojects/bounded_context/place_search/infra/src/integrationTest/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/ListConqueredPlacesTest.kt
@@ -1,15 +1,23 @@
 package club.staircrusher.place_search.infra.adapter.`in`.controller
 
+import club.staircrusher.accessibility.domain.model.PlaceAccessibility
+import club.staircrusher.api.spec.dto.DeleteAccessibilityPostRequest
 import club.staircrusher.api.spec.dto.ListConqueredPlacesResponseDto
 import club.staircrusher.place_search.infra.adapter.`in`.controller.base.PlaceSearchITBase
+import club.staircrusher.testing.spring_it.mock.MockSccClock
 import com.fasterxml.jackson.core.type.TypeReference
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.time.Duration
 
 
 class ListConqueredPlacesTest : PlaceSearchITBase() {
+    @Autowired
+    lateinit var mockSccClock: MockSccClock
+
     @Test
     fun testListConqueredPlaces() {
         val user = transactionManager.doInTransaction {
@@ -36,6 +44,34 @@ class ListConqueredPlacesTest : PlaceSearchITBase() {
                     assertTrue(item.hasBuildingAccessibility)
                     assertNull(item.distanceMeters)
                 }
+            }
+    }
+
+    @Test
+    fun 생성_시간_역순으로_내려준다() {
+        val user = transactionManager.doInTransaction {
+            testDataGenerator.createUser()
+        }
+
+        val placeIdsOrderedByCreatedAtDesc = (1..10).map {
+            mockSccClock.advanceTime(Duration.ofHours(1))
+            transactionManager.doInTransaction {
+                val place = testDataGenerator.createBuildingAndPlace()
+                val (pa, _) = testDataGenerator.registerBuildingAndPlaceAccessibility(place, user)
+                place to pa
+            }
+        }
+            .sortedByDescending { it.second.createdAt }
+            .map { it.first.id }
+
+        mvc
+            .sccRequest("/listConqueredPlaces", requestBody = null, user = user)
+            .apply {
+                val result = getResult(object : TypeReference<ListConqueredPlacesResponseDto>() {})
+                assertEquals(
+                    placeIdsOrderedByCreatedAtDesc,
+                    result.items.map { it.place.id },
+                )
             }
     }
 }
