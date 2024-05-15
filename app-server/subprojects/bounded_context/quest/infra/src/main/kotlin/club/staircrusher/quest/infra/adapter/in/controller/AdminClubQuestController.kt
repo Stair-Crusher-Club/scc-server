f3e445d7b0600b8c23db6eb636bead94a31c7b57
Zeniuus <jsh56son@gmail.com>
2024-05-15 14:14:22 +0900

    마이그레이션 코드 제거

--- app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/in/controller/AdminClubQuestController.kt
+++ app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/in/controller/AdminClubQuestController.kt
@@ -10,7 +10,6 @@ import club.staircrusher.admin_api.spec.dto.ClubQuestsGet200ResponseInner
 import club.staircrusher.admin_api.spec.dto.CreateClubQuestRequest
 import club.staircrusher.admin_api.spec.dto.CreateClubQuestResponseDTO
 import club.staircrusher.quest.application.port.`in`.ClubQuestCreateAplService
-import club.staircrusher.quest.application.port.`in`.ClubQuestMigrateEntitiesUseCase
 import club.staircrusher.quest.application.port.`in`.ClubQuestSetIsClosedUseCase
 import club.staircrusher.quest.application.port.`in`.ClubQuestSetIsNotAccessibleUseCase
 import club.staircrusher.quest.application.port.`in`.CrossValidateClubQuestPlacesUseCase
@@ -37,7 +36,6 @@ class AdminClubQuestController(
     private val deleteClubQuestUseCase: DeleteClubQuestUseCase,
     private val clubQuestRepository: ClubQuestRepository,
     private val crossValidateClubQuestPlacesUseCase: CrossValidateClubQuestPlacesUseCase,
-    private val clubQuestMigrateEntitiesUseCase: ClubQuestMigrateEntitiesUseCase,
 ) {
     @GetMapping("/admin/clubQuests")
     fun listClubQuests(): List<ClubQuestsGet200ResponseInner> {
@@ -110,10 +108,5 @@ class AdminClubQuestController(
     @PutMapping("/admin/clubQuests/{clubQuestId}/crossValidate")
     fun crossValidate(@PathVariable clubQuestId: String) {
         return crossValidateClubQuestPlacesUseCase.handle(clubQuestId)
-    }
-
-    @PutMapping("/admin/clubQuests/migrateEntities")
-    fun migrateEntities() {
-        return clubQuestMigrateEntitiesUseCase.handle()
     }
 }
