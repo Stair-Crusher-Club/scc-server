52fa9f418c982454f1d6b0cf11071c94038ada11
Zeniuus <jsh56son@gmail.com>
2024-05-18 14:43:27 +0900

    Fix: 퀘스트 화면에서 건물 번호와 핀 번호가 어긋나는 문제 해결

--- app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/out/persistence/Converters.kt
+++ app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/out/persistence/Converters.kt
@@ -23,7 +23,7 @@ fun Club_quest.toDomainModel(targetBuildings: List<ClubQuestTargetBuilding>) = C
     id = id,
     name = name,
     questCenterLocation = Location(lng = quest_center_location_x, lat = quest_center_location_y),
-    targetBuildings = targetBuildings,
+    targetBuildings = targetBuildings.sortedBy { it.name },
     createdAt = created_at.toInstant(),
     updatedAt = updated_at.toInstant(),
 )
