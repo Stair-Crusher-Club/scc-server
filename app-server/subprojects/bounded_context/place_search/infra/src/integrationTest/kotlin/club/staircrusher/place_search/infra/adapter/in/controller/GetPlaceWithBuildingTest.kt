f74779b71989547f34037b2d0ec24157dcba38bf
sanggggg <37951125+sanggggg@users.noreply.github.com>
2024-05-19 21:34:57 +0900

    get place with building

---
+++ app-server/subprojects/bounded_context/place_search/infra/src/integrationTest/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/GetPlaceWithBuildingTest.kt
@@ -0,0 +1,39 @@
+package club.staircrusher.place_search.infra.adapter.`in`.controller
+
+import club.staircrusher.api.spec.dto.GetAccessibilityPostRequest
+import club.staircrusher.api.spec.dto.ListPlacesInBuildingPostRequest
+import club.staircrusher.api.spec.dto.PlaceListItem
+import club.staircrusher.place_search.infra.adapter.`in`.controller.base.PlaceSearchITBase
+import club.staircrusher.stdlib.testing.SccRandom
+import com.fasterxml.jackson.core.type.TypeReference
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class GetPlaceWithBuildingTest : PlaceSearchITBase() {
+    @Test
+    fun 정상적인동작() {
+        val user = transactionManager.doInTransaction {
+            testDataGenerator.createUser()
+        }
+        val (building, place) = transactionManager.doInTransaction {
+            val building = testDataGenerator.createBuilding()
+            testDataGenerator.registerBuildingAccessibilityIfNotExists(building)
+            val place = testDataGenerator.createPlace(placeName = SccRandom.string(32), building = building)
+            testDataGenerator.registerPlaceAccessibility(place)
+            Pair(building, place)
+        }
+
+        val params = GetAccessibilityPostRequest(
+            placeId = place.id
+        )
+        mvc
+            .sccRequest("/getPlaceWithBuilding", params, user = user)
+            .apply {
+                val result = getResult(object : TypeReference<PlaceListItem>() {})
+                assertEquals(place.id, result.place.id)
+                assertEquals(building.id, result.building.id)
+            }
+    }
+}
