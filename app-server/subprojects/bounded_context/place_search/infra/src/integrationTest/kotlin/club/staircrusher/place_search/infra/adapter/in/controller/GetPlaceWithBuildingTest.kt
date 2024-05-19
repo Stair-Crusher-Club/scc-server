65728c727970d0a3b15c2f07a31f24d22a0efcc6
sanggggg <37951125+sanggggg@users.noreply.github.com>
2024-05-19 21:42:03 +0900

    fix

--- app-server/subprojects/bounded_context/place_search/infra/src/integrationTest/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/GetPlaceWithBuildingTest.kt
+++ app-server/subprojects/bounded_context/place_search/infra/src/integrationTest/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/GetPlaceWithBuildingTest.kt
@@ -1,14 +1,11 @@
 package club.staircrusher.place_search.infra.adapter.`in`.controller
 
 import club.staircrusher.api.spec.dto.GetAccessibilityPostRequest
-import club.staircrusher.api.spec.dto.ListPlacesInBuildingPostRequest
 import club.staircrusher.api.spec.dto.PlaceListItem
 import club.staircrusher.place_search.infra.adapter.`in`.controller.base.PlaceSearchITBase
 import club.staircrusher.stdlib.testing.SccRandom
 import com.fasterxml.jackson.core.type.TypeReference
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class GetPlaceWithBuildingTest : PlaceSearchITBase() {
