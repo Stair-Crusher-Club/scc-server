c7be8c1dacebfce6c6bf0955e8ec80870cd8e11e
Suhwan Jee <jsh56son@gmail.com>
2024-05-25 10:37:23 +0900

    퀘스트 관련 불필요한 column들 drop (#304)
    * 퀘스트 리팩토링 4 - 불필요한 json column drop
    
    * ClubQuestTargetPlace에서 폐업 / 접근 불가 여부 column drop

--- app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/out/persistence/Converters.kt
+++ app-server/subprojects/bounded_context/quest/infra/src/main/kotlin/club/staircrusher/quest/infra/adapter/out/persistence/Converters.kt
@@ -14,7 +14,6 @@ fun ClubQuest.toPersistenceModel() = Club_quest(
     name = name,
     quest_center_location_x = questCenterLocation.lng,
     quest_center_location_y = questCenterLocation.lat,
-    target_buildings = emptyList(),
     created_at = createdAt.toOffsetDateTime(),
     updated_at = updatedAt.toOffsetDateTime(),
 )
@@ -59,8 +58,6 @@ fun ClubQuestTargetPlace.toPersistenceModel() = Club_quest_target_place(
     name = name,
     location_x = location.lng,
     location_y = location.lat,
-    is_closed = false,
-    is_not_accessible = false,
     created_at = createdAt.toOffsetDateTime(),
     updated_at = updatedAt.toOffsetDateTime(),
 )
