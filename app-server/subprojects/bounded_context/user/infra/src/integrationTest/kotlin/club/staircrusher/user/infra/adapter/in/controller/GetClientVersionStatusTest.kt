bde76eb81c505970e2a6d34ac5c8668667566afc
yongseongkim <yongseongkimm@gmail.com>
2024-05-27 00:06:57 +0900

    beforeEach 사용하기

--- app-server/subprojects/bounded_context/user/infra/src/integrationTest/kotlin/club/staircrusher/user/infra/adapter/in/controller/GetClientVersionStatusTest.kt
+++ app-server/subprojects/bounded_context/user/infra/src/integrationTest/kotlin/club/staircrusher/user/infra/adapter/in/controller/GetClientVersionStatusTest.kt
@@ -28,13 +28,6 @@ class GetClientVersionStatusTest : SccSpringITBase() {
 
     @Test
     fun `클라이언트 버전이 너무 낮으면 강제 업데이트를 안내한다`() {
-        Mockito.`when`(
-            clientVersionService.getUpgradedNeededVersion()
-        ).thenReturn(SemanticVersion(0, 1, 0))
-        Mockito.`when`(
-            clientVersionService.getUpgradedRecommendedVersion()
-        ).thenReturn(SemanticVersion(0, 2, 0))
-
         val status = mvc.sccRequest(
             "/getClientVersionStatus",
             GetClientVersionStatusRequestDto(version = "0.0.9")
@@ -46,13 +39,6 @@ class GetClientVersionStatusTest : SccSpringITBase() {
 
     @Test
     fun `클라이언트 버전이 낮으면 업데이트 추천을 안내한다`() {
-        Mockito.`when`(
-            clientVersionService.getUpgradedNeededVersion()
-        ).thenReturn(SemanticVersion(0, 1, 0))
-        Mockito.`when`(
-            clientVersionService.getUpgradedRecommendedVersion()
-        ).thenReturn(SemanticVersion(0, 2, 0))
-
         val status = mvc.sccRequest(
             "/getClientVersionStatus",
             GetClientVersionStatusRequestDto(version = "0.1.5")
