7c0d3a00b55dcad56be75cc0110af735e97dcd11
yongseongkim <yongseongkimm@gmail.com>
2024-06-18 23:45:13 +0900

    Make lint success

--- app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AccessibilityImagePostProcessController.kt
+++ app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/controller/AccessibilityImagePostProcessController.kt
@@ -3,8 +3,6 @@ package club.staircrusher.accessibility.infra.adapter.`in`.controller
 import club.staircrusher.accessibility.application.port.`in`.BlurFacesInAccessibilityImagesUseCase
 import club.staircrusher.accessibility.application.port.`in`.BlurFacesInLatestAccessibilityImagesUseCase
 import com.fasterxml.jackson.annotation.JsonProperty
-import jakarta.servlet.http.HttpServletRequest
-import org.springframework.security.web.util.matcher.IpAddressMatcher
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -16,30 +14,16 @@ class AccessibilityImagePostProcessController(
 ) {
     @PostMapping("/blurFacesInAccessibilityImages")
     fun blurFacesInAccessibilityImages(
-        request: HttpServletRequest,
         @RequestBody params: BlurFacesInAccessibilityImages,
     ) {
-        // TODO: 잘 돌아가는지 확인 후 authority 확인
-        // checkAuthority(request)
+        // TODO: UpdateChallengeRank 처럼 IP 체크
         blurFacesInAccessibilityImagesUseCase.handleAsync(params.placeAccessibilityId)
     }
 
     @PostMapping("/blurFacesInLatestPlaceAccessibilityImages")
-    fun blurFacesInLatestPlaceAccessibilityImages(request: HttpServletRequest) {
-        // TODO: 잘 돌아가는지 확인 후 authority 확인
-        // checkAuthority(request)
+    fun blurFacesInLatestPlaceAccessibilityImages() {
+        // TODO: UpdateChallengeRank 처럼 IP 체크
         blurFacesInLatestAccessibilityImagesUseCase.handleAsync()
-    }
-
-    private fun checkAuthority(request: HttpServletRequest) {
-        val clusterIpAddressMatcher = IpAddressMatcher("10.42.0.0/16")
-        val localIpAddressMatcher = IpAddressMatcher("127.0.0.1/32")
-        if (
-            !clusterIpAddressMatcher.matches(request)
-            && !localIpAddressMatcher.matches(request)
-        ) {
-            throw IllegalArgumentException("Unauthorized")
-        }
     }
 }
 
