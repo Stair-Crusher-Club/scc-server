525c58fbc327dbd3cfe5589bc922f9c21c1cbdfc
Jason Yoo <jasonyoo0515@gmail.com>
2024-06-19 23:42:32 +0900

    chore: log for debug

--- app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/cdn/SccCdn.kt
+++ app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/cdn/SccCdn.kt
@@ -1,27 +1,33 @@
 package club.staircrusher.spring_web.cdn
 
 import club.staircrusher.stdlib.di.annotation.Component
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 
 @Component
 open class SccCdn(
     @Value("\${scc.cloudfront.domain:#{null}") val domain: String?,
 ) {
+    private val logger = KotlinLogging.logger {}
+
     init {
         SccCdnBeanHolder.setIfNull(this)
     }
 
     @Suppress("ReturnCount")
     fun replaceIfPossible(url: String): String {
+        logger.info("Replacing CDN URL: $url")
         if (domain == null) {
             return url
         }
 
         val objectKey = url.substringAfterLast(S3_DOMAIN)
+        logger.info("Object key: $objectKey")
         if (objectKey == url) {
             return url
         }
 
+        logger.info("Replaced URL: https://$domain/$objectKey")
         return "https://$domain/$objectKey"
     }
 
