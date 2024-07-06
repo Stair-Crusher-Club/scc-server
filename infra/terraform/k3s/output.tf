ec89e59011d30af163c8456c101a57280185e166
Jason Yoo <jasonyoo0515@gmail.com>
2024-07-06 14:32:21 +0900

    chore: sync tf state

--- infra/terraform/k3s/output.tf
+++ infra/terraform/k3s/output.tf
@@ -5,7 +5,3 @@ output "lb_dns_name" {
 output "k3s_ip" {
   value = aws_lightsail_instance.k3s_control_plane.public_ip_address
 }
-
-output "db_endpoint" {
-  value = aws_lightsail_database.scc_db.master_endpoint_address
-}
