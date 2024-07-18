1130394bb20afdb4f5a1a1081e1510d0522e3167
Jason Yoo <jasonyoo0515@gmail.com>
2024-07-18 23:31:15 +0900

    feat: remove email on delete user

--- app-server/subprojects/bounded_context/user/domain/src/main/kotlin/club/staircrusher/user/domain/model/User.kt
+++ app-server/subprojects/bounded_context/user/domain/src/main/kotlin/club/staircrusher/user/domain/model/User.kt
@@ -7,7 +7,7 @@ data class User(
     var nickname: String,
     @Deprecated("닉네임 로그인은 사라질 예정") var encryptedPassword: String?,
     var instagramId: String?,
-    var email: String?, // FIXME: 레거시 계정이 모두 사라지면 non-nullable로 변경
+    var email: String?,
     val mobilityTools: MutableList<UserMobilityTool>,
     val createdAt: Instant,
 ) {
@@ -17,6 +17,7 @@ data class User(
         get() = deletedAt != null
 
     fun delete(deletedAt: Instant) {
+        this.email = null
         this.deletedAt = deletedAt
     }
 }
