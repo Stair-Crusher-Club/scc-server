668bc050d2e0091bed4aea9800aaf69a6f578e6f
Jason Yoo <jasonyoo0515@gmail.com>
2024-07-19 00:26:54 +0900

    chore: do not include type in payload

--- app-server/subprojects/cross_cutting_concern/domain/server_event/src/main/kotlin/club/staircrusher/domain/server_event/ServerEventPayload.kt
+++ app-server/subprojects/cross_cutting_concern/domain/server_event/src/main/kotlin/club/staircrusher/domain/server_event/ServerEventPayload.kt
@@ -1,5 +1,6 @@
 package club.staircrusher.domain.server_event
 
 interface ServerEventPayload {
-    fun getType(): ServerEventType
+    // getType 으로 이름을 붙이면 jackson 에서 getter 로 인식하면서 함께 직렬화가 된다
+    fun type(): ServerEventType
 }
