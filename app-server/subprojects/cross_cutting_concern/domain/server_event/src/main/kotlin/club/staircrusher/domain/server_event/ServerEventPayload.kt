816e5271be0c78f24f37be99c19d1a76b9cf1558
Jason Yoo <jasonyoo0515@gmail.com>
2024-07-19 23:14:10 +0900

    feat: use jsonignore annotation

--- app-server/subprojects/cross_cutting_concern/domain/server_event/src/main/kotlin/club/staircrusher/domain/server_event/ServerEventPayload.kt
+++ app-server/subprojects/cross_cutting_concern/domain/server_event/src/main/kotlin/club/staircrusher/domain/server_event/ServerEventPayload.kt
@@ -1,6 +1,8 @@
 package club.staircrusher.domain.server_event
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 interface ServerEventPayload {
-    // getType 으로 이름을 붙이면 jackson 에서 getter 로 인식하면서 함께 직렬화가 된다
-    fun type(): ServerEventType
+    @get:JsonIgnore
+    val type: ServerEventType
 }
