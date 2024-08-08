7e6c87814922152ae7cde85b7dd6cd2ee7cec4e6
yongseongkim <yongseongkimm@gmail.com>
2024-08-09 00:00:03 +0900

    Blur parallely

--- app-server/subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/AccessibilityImageFaceBlurringService.kt
+++ app-server/subprojects/bounded_context/accessibility/application/src/main/kotlin/club/staircrusher/accessibility/application/port/in/AccessibilityImageFaceBlurringService.kt
@@ -5,6 +5,9 @@ import club.staircrusher.accessibility.application.port.out.DetectFacesService
 import club.staircrusher.accessibility.application.port.out.file_management.FileManagementService
 import club.staircrusher.stdlib.di.annotation.Component
 import club.staircrusher.stdlib.persistence.TransactionManager
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import mu.KotlinLogging
 
 @Component
@@ -37,43 +40,45 @@ open class AccessibilityImageFaceBlurringService(
         return BuildingAccessibilityBlurResult(entranceResult, elevatorResult)
     }
 
-    private suspend fun detectAndBlurFaces(imageUrls: List<String>): List<BlurResult> {
-        return imageUrls.map { imageUrl ->
-            try {
-                val (name, extension) = imageUrl.split("/").last().split(".")
-                if (listOf("jpg", "jpeg", "png", "webp").contains(extension).not()) {
-                    logger.info { "Detecting and blurring faces failed. $imageUrl is not image." }
-                    return@map BlurResult(
-                        originalImageUrl = imageUrl,
-                        blurredImageUrl = imageUrl,
-                        detectedPeopleCount = 0
-                    )
-                }
-                val detected = detectFacesService.detect(imageUrl)
-                val imageBytes = detected.imageBytes
-                if (detected.positions.isEmpty()) return@map BlurResult(
-                    originalImageUrl = imageUrl,
-                    blurredImageUrl = imageUrl,
-                    detectedPeopleCount = 0
-                )
-                val (blurredImageUrl, detectedPositions) = run {
-                    val outputByteArray = imageProcessor.blur(imageBytes, extension, detected.positions)
-                    val blurredImageUrl = fileManagementService.uploadImage("${name}_b.$extension", outputByteArray)
-                    blurredImageUrl to detected.positions
-                }
-                return@map BlurResult(
-                    originalImageUrl = imageUrl,
-                    blurredImageUrl = blurredImageUrl ?: imageUrl,
-                    detectedPeopleCount = detectedPositions.size
-                )
-            } catch (e: Exception) {
-                logger.error(e) { "Detecting and blurring faces in the image($imageUrl) failed." }
-                return@map BlurResult(
+    private suspend fun detectAndBlurFaces(imageUrls: List<String>): List<BlurResult> = coroutineScope {
+        imageUrls.map { async { detectAndBlurFaces(it) } }.awaitAll()
+    }
+
+    private suspend fun detectAndBlurFaces(imageUrl: String): BlurResult {
+        try {
+            val (name, extension) = imageUrl.split("/").last().split(".")
+            if (listOf("jpg", "jpeg", "png", "webp").contains(extension).not()) {
+                logger.info { "Detecting and blurring faces failed. $imageUrl is not image." }
+                return BlurResult(
                     originalImageUrl = imageUrl,
                     blurredImageUrl = imageUrl,
                     detectedPeopleCount = 0
                 )
             }
+            val detected = detectFacesService.detect(imageUrl)
+            val imageBytes = detected.imageBytes
+            if (detected.positions.isEmpty()) return BlurResult(
+                originalImageUrl = imageUrl,
+                blurredImageUrl = imageUrl,
+                detectedPeopleCount = 0
+            )
+            val (blurredImageUrl, detectedPositions) = run {
+                val outputByteArray = imageProcessor.blur(imageBytes, extension, detected.positions)
+                val blurredImageUrl = fileManagementService.uploadImage("${name}_b.$extension", outputByteArray)
+                blurredImageUrl to detected.positions
+            }
+            return BlurResult(
+                originalImageUrl = imageUrl,
+                blurredImageUrl = blurredImageUrl ?: imageUrl,
+                detectedPeopleCount = detectedPositions.size
+            )
+        } catch (e: Exception) {
+            logger.error(e) { "Detecting and blurring faces in the image($imageUrl) failed." }
+            return BlurResult(
+                originalImageUrl = imageUrl,
+                blurredImageUrl = imageUrl,
+                detectedPeopleCount = 0
+            )
         }
     }
 
