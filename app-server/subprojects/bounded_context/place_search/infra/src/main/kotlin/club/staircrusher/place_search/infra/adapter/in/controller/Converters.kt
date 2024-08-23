6d46941fe99711c2e23a4d4dff5b9e9c5888e27e
sanggggg <37951125+sanggggg@users.noreply.github.com>
2024-08-23 22:01:52 +0900

    category info

--- app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/Converters.kt
+++ app-server/subprojects/bounded_context/place_search/infra/src/main/kotlin/club/staircrusher/place_search/infra/adapter/in/controller/Converters.kt
@@ -12,6 +12,7 @@ fun Place.toDTO() = club.staircrusher.api.spec.dto.Place(
     id = id,
     name = name,
     address = address.toString(),
+    category = category?.toDto(),
 )
 
 fun Building.toDTO() = club.staircrusher.api.spec.dto.Building(
