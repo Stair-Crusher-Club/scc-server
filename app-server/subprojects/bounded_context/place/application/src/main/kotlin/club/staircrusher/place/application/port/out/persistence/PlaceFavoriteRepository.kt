e17c9617a4059724f32f88a2c70a60c99f85c5bc
Jason Yoo <jasonyoo0515@gmail.com>
2024-09-06 00:21:31 +0900

    Revert "chore: curd -> jpa repository"
    This reverts commit 557efde339dd38dbc7d7de23b990e9eaff8dbd50.

--- app-server/subprojects/bounded_context/place/application/src/main/kotlin/club/staircrusher/place/application/port/out/persistence/PlaceFavoriteRepository.kt
+++ app-server/subprojects/bounded_context/place/application/src/main/kotlin/club/staircrusher/place/application/port/out/persistence/PlaceFavoriteRepository.kt
@@ -1,9 +1,9 @@
 package club.staircrusher.place.application.port.out.persistence
 
 import club.staircrusher.place.domain.model.PlaceFavorite
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 
-interface PlaceFavoriteRepository : JpaRepository<PlaceFavorite, String> {
+interface PlaceFavoriteRepository : CrudRepository<PlaceFavorite, String> {
     fun findByUserIdAndDeletedAtIsNull(userId: String): List<PlaceFavorite>
     fun findFirstByUserIdAndPlaceIdAndDeletedAtIsNull(userId: String, placeId: String): PlaceFavorite?
     fun findFirstByUserIdAndPlaceId(userId: String, placeId: String): PlaceFavorite?
