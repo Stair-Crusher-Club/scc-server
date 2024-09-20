8a8c21785dd5c3fc69cc8dd121e2f1eecc7eaef8
Jason Yoo <jasonyoo0515@gmail.com>
2024-09-20 18:29:48 +0900

    chore: remove unnecessary library

--- app-server/subprojects/bounded_context/place/application/build.gradle.kts
+++ app-server/subprojects/bounded_context/place/application/build.gradle.kts
@@ -6,5 +6,4 @@ dependencies {
     val jtsVersion: String by project
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.locationtech.jts:jts-core:$jtsVersion")
-    implementation("info.debatty:java-string-similarity:2.0.0")
 }
