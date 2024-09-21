ed10273fb6252db84a6ba2e08420fccd6c668a90
Jason Yoo <jasonyoo0515@gmail.com>
2024-09-21 23:19:49 +0900

    feat: closed candidate field added

--- app-server/subprojects/bounded_context/place/domain/src/main/kotlin/club/staircrusher/place/domain/model/ClosedPlaceCandidate.kt
+++ app-server/subprojects/bounded_context/place/domain/src/main/kotlin/club/staircrusher/place/domain/model/ClosedPlaceCandidate.kt
@@ -4,6 +4,7 @@ import club.staircrusher.stdlib.persistence.jpa.TimeAuditingBaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import java.time.Instant
 
 @Entity
 class ClosedPlaceCandidate(
@@ -12,6 +13,12 @@ class ClosedPlaceCandidate(
 
     @Column(nullable = false)
     val placeId: String,
+
+    @Column(nullable = true)
+    var acceptedAt: Instant? = null,
+
+    @Column(nullable = true)
+    var ignoredAt: Instant? = null,
 ) : TimeAuditingBaseEntity() {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
