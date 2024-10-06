ce11c0f549b21ca45824545f448a7568e3db30d7
Jason Yoo <jasonyoo0515@gmail.com>
2024-10-06 15:14:15 +0900

    refactor: internal ip address checker

--- app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/PlaceController.kt
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/PlaceController.kt
@@ -1,8 +1,8 @@
 package club.staircrusher.place.infra.adapter.`in`.controller
 
 import club.staircrusher.place.application.port.`in`.CreateClosedPlaceCandidatesUseCase
+import club.staircrusher.spring_web.security.InternalIpAddressChecker
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.security.web.util.matcher.IpAddressMatcher
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,14 +12,7 @@ class PlaceController(
 ) {
     @PostMapping("/createClosedPlaceCandidates")
     fun createClosedPlaceCandidates(request: HttpServletRequest) {
-        val clusterIpAddressMatcher = IpAddressMatcher("10.42.0.0/16")
-        val localIpAddressMatcher = IpAddressMatcher("127.0.0.1/32")
-        if (
-            !clusterIpAddressMatcher.matches(request)
-            && !localIpAddressMatcher.matches(request)
-        ) {
-            throw IllegalArgumentException("Unauthorized")
-        }
+        InternalIpAddressChecker.check(request)
         createClosedPlaceCandidatesUseCase.handle()
     }
 }
