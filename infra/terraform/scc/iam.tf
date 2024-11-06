5f6c2cf44d92a033f4911f1ea43ffe88070b3b9c
SuhwanJee <suhwan@rapportlabs.kr>
2024-11-06 09:16:16 +0900

    Fix: bucket 권한 설정 올바르게

--- infra/terraform/scc/iam.tf
+++ infra/terraform/scc/iam.tf
@@ -40,6 +40,18 @@ data "aws_iam_policy_document" "scc_accessibility_thumbnails_full_access" {
   }
 }
 
+data "aws_iam_policy_document" "scc_home_banners_full_access" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      aws_s3_bucket.home_banners.arn,
+      "${aws_s3_bucket.home_banners.arn}/*",
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "scc_rekognition_access" {
   statement {
     actions = [
@@ -64,6 +76,11 @@ resource "aws_iam_policy" "scc_accessibility_thumbnails_full_access" {
   policy = data.aws_iam_policy_document.scc_accessibility_thumbnails_full_access.json
 }
 
+resource "aws_iam_policy" "scc_home_banners_full_access" {
+  name   = "scc-home-banners-full-access"
+  policy = data.aws_iam_policy_document.scc_home_banners_full_access.json
+}
+
 resource "aws_iam_policy" "scc_rekognition_access" {
   name   = "scc-rekognition-access"
   policy = data.aws_iam_policy_document.scc_rekognition_access.json
@@ -77,6 +94,11 @@ resource "aws_iam_role_policy_attachment" "scc_accessibility_images_full_access"
 resource "aws_iam_role_policy_attachment" "scc_accessibility_thumbnails_full_access" {
   role       = aws_iam_role.scc.name
   policy_arn = aws_iam_policy.scc_accessibility_thumbnails_full_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "scc_home_banners_full_access" {
+  role       = aws_iam_role.scc.name
+  policy_arn = aws_iam_policy.scc_home_banners_full_access.arn
 }
 
 resource "aws_iam_role_policy_attachment" "scc_rekognition_access" {
