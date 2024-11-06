01effb4d26c98855934eec5a545c36c122ce984d
Jason Yoo <jasonyoo0515@gmail.com>
2024-11-06 18:24:38 +0900

    chore: update string similarity comparison algorithm

--- app-server/subprojects/bounded_context/external_accessibility/infra/build.gradle.kts
+++ app-server/subprojects/bounded_context/external_accessibility/infra/build.gradle.kts
@@ -4,19 +4,19 @@ plugins {
 }
 
 dependencies {
+    val awsSdkVersion: String by project
+    val apacheCommonsVersion: String by project
+
     api(projects.apiSpecification.api)
     api(projects.apiSpecification.domainEvent)
     implementation(projects.crossCuttingConcern.infra.persistenceModel)
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("io.projectreactor.netty:reactor-netty")
     implementation("org.springframework:spring-webflux")
-    implementation("org.apache.commons:commons-csv:1.11.0")
-
-    integrationTestImplementation(projects.crossCuttingConcern.test.springIt)
-
-    val awsSdkVersion: String by project
+    implementation("org.apache.commons:commons-csv:$apacheCommonsVersion")
     implementation("software.amazon.awssdk:s3:$awsSdkVersion")
     runtimeOnly("software.amazon.awssdk:sts:$awsSdkVersion") // IRSA를 사용하기 위해서 필요함
 
+    integrationTestImplementation(projects.crossCuttingConcern.test.springIt)
     testImplementation(projects.boundedContext.externalAccessibility.application)
 }
