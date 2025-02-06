74129db633535bd9780415f5f73d4fff4d9295af
Jason Yoo <jasonyoo0515@gmail.com>
2025-02-06 22:40:39 +0900

    fix: user account connection table name

--- app-server/subprojects/bounded_context/user/domain/src/main/kotlin/club/staircrusher/user/domain/model/UserAccountConnection.kt
+++ app-server/subprojects/bounded_context/user/domain/src/main/kotlin/club/staircrusher/user/domain/model/UserAccountConnection.kt
@@ -3,10 +3,8 @@ package club.staircrusher.user.domain.model
 import club.staircrusher.stdlib.persistence.jpa.TimeAuditingBaseEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
-import jakarta.persistence.Table
 
 @Entity
-@Table(name = "scc_user_account_connection")
 class UserAccountConnection(
     @Id
     val id: String,
