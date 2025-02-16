cf611ad2967c6913e942aa5054477bf73b4ce4f5
Jason Yoo <jasonyoo0515@gmail.com>
2025-02-16 23:58:19 +0900

    fix: id length

--- app-server/subprojects/cross_cutting_concern/infra/persistence_model/src/main/resources/db/migration/V37__add_user_account_id_to_profile.sql
+++ app-server/subprojects/cross_cutting_concern/infra/persistence_model/src/main/resources/db/migration/V37__add_user_account_id_to_profile.sql
@@ -1,3 +1,3 @@
-ALTER TABLE scc_user ADD COLUMN user_account_id VARCHAR(32) NULL;
+ALTER TABLE scc_user ADD COLUMN user_account_id VARCHAR(36) NULL;
 
 CREATE INDEX idx_user_table_3 ON scc_user(user_account_id);
