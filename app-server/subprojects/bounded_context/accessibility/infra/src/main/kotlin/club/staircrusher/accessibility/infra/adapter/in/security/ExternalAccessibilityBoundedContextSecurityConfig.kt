20ec46680315c735b01127b1bbb18a6991fc6ec5
Jason Yoo <jasonyoo0515@gmail.com>
2025-02-20 19:40:11 +0900

    chore: remove default emptylist

--- app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/security/ExternalAccessibilityBoundedContextSecurityConfig.kt
+++ app-server/subprojects/bounded_context/accessibility/infra/src/main/kotlin/club/staircrusher/accessibility/infra/adapter/in/security/ExternalAccessibilityBoundedContextSecurityConfig.kt
@@ -3,10 +3,13 @@ package club.staircrusher.accessibility.infra.adapter.`in`.security
 import club.staircrusher.spring_web.security.SccSecurityConfig
 import club.staircrusher.stdlib.di.annotation.Component
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 @Component
 class ExternalAccessibilityBoundedContextSecurityConfig : SccSecurityConfig {
     override fun requestMatchers() = listOf(
         "/admin/syncWithDataSource"
     ).map { AntPathRequestMatcher(it) }
+
+    override fun identifiedUserOnlyRequestMatchers() = emptyList<RequestMatcher>()
 }
