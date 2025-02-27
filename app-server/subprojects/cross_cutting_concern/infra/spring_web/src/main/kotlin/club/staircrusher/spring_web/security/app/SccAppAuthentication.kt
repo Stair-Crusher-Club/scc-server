c2db02b43d33b0827378f75a4ab82f5cf49651f4
Jason Yoo <jasonyoo0515@gmail.com>
2025-02-28 02:20:44 +0900

    Revert "chore: add security logging"
    This reverts commit d7c962279114522f44d07261eb79e990fdc38831.

--- app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/app/SccAppAuthentication.kt
+++ app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/app/SccAppAuthentication.kt
@@ -1,7 +1,6 @@
 package club.staircrusher.spring_web.security.app
 
 import club.staircrusher.stdlib.auth.AuthUser
-import mu.KotlinLogging
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -9,15 +8,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 class SccAppAuthentication(
     private val authUser: AuthUser,
 ) : Authentication {
-    private val logger = KotlinLogging.logger {}
-
     override fun getName(): String? {
         return null
     }
 
     override fun getAuthorities(): Collection<GrantedAuthority> {
         val roles = setOf("ROLE_ANONYMOUS", "ROLE_${authUser.type}")
-        logger.info { "Get Authorities triggered, roles of user(${authUser.id}) is $roles" }
         return roles.map { SimpleGrantedAuthority(it) }
     }
 
--- app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/app/SccAppAuthenticationProvider.kt
+++ app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/app/SccAppAuthenticationProvider.kt
@@ -5,7 +5,6 @@ import club.staircrusher.stdlib.auth.AuthUser
 import club.staircrusher.user.application.port.`in`.UserApplicationService
 import club.staircrusher.user.application.port.`in`.UserAuthApplicationService
 import club.staircrusher.user.domain.exception.UserAuthenticationException
-import mu.KotlinLogging
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.Authentication
@@ -16,18 +15,14 @@ class SccAppAuthenticationProvider(
     private val userAuthApplicationService: UserAuthApplicationService,
     private val userApplicationService: UserApplicationService,
 ) : AuthenticationProvider {
-    private val logger = KotlinLogging.logger {}
-
     override fun authenticate(authentication: Authentication): Authentication {
         val beforeAuthSccAuthentication = authentication as BeforeAuthSccAuthentication
         val accessToken = beforeAuthSccAuthentication.credentials
-        logger.info { "AccessToken: $accessToken" }
         val userId = try {
             userAuthApplicationService.verify(accessToken)
         } catch (e: UserAuthenticationException) {
             throw BadCredentialsException("Invalid access token.", e)
         }
-        logger.info { "UserId: $userId" }
 
         val user = userApplicationService.getAccountOrNull(userId)
             ?: throw BadCredentialsException("No User found with given credentials.")
@@ -35,8 +30,6 @@ class SccAppAuthenticationProvider(
             throw BadCredentialsException("No User found with given credentials.")
         }
         val userProfile = userApplicationService.getProfileByUserIdOrNull(userId)
-
-        logger.info { "UserAccountType: ${user.accountType.name}" }
 
         return SccAppAuthentication(
             AuthUser(
