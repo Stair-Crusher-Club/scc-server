de5b0c17e74e38a02b71fae7d0289c36e68d47f0
Jason Yoo <jasonyoo0515@gmail.com>
2025-02-28 02:34:24 +0900

    Revert "chore: log result of kakao login"
    This reverts commit 6b09694d576918d94c900c9d14c36bc1d04612bc.

--- app-server/subprojects/bounded_context/user/infra/src/main/kotlin/club/staircrusher/user/infra/adapter/in/controller/AuthController.kt
+++ app-server/subprojects/bounded_context/user/infra/src/main/kotlin/club/staircrusher/user/infra/adapter/in/controller/AuthController.kt
@@ -14,7 +14,6 @@ import club.staircrusher.user.application.port.`in`.use_case.LoginWithAppleUseCa
 import club.staircrusher.user.application.port.`in`.use_case.LoginWithKakaoUseCase
 import club.staircrusher.user.domain.model.UserAccountType
 import club.staircrusher.user.infra.adapter.`in`.converter.toDTO
-import mu.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -27,8 +26,6 @@ class AuthController(
     private val loginWithAppleUseCase: LoginWithAppleUseCase,
     private val createAnonymousUserUseCase: CreateAnonymousUserUseCase,
 ) {
-    private val logger = KotlinLogging.logger {}
-
     @PostMapping("/signUp")
     @Deprecated(message = "소셜 로그인으로 대체됨", replaceWith = ReplaceWith("loginWithKakao"))
     fun signUp(@RequestBody request: SignUpPostRequest): ResponseEntity<Unit> {
@@ -59,9 +56,7 @@ class AuthController(
     @PostMapping("/loginWithKakao")
     fun loginWithKakao(@RequestBody request: LoginWithKakaoPostRequest, authentication: SccAppAuthentication?): LoginResultDto {
         val anonymousUserId = authentication?.details?.takeIf { it.type == UserAccountType.ANONYMOUS.name }?.id
-        val result = loginWithKakaoUseCase.handle(request.kakaoTokens.refreshToken, request.kakaoTokens.idToken, anonymousUserId)
-        logger.info { "loginWithKakao result for anonymous user($anonymousUserId): $result" }
-        return result.toDTO()
+        return loginWithKakaoUseCase.handle(request.kakaoTokens.refreshToken, request.kakaoTokens.idToken, anonymousUserId).toDTO()
     }
 
     @PostMapping("/loginWithApple")
