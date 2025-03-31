3ebbe96d22a7ae7fe22989e0f31c4eee4c17ded0
Jason Yoo <jasonyoo0515@gmail.com>
2025-03-31 23:37:04 +0900

    feat: add more metric alarms

---
+++ infra/terraform/monitoring/alarms.tf
@@ -0,0 +1,90 @@
+variable "alarms" {
+  description = "Map of alarms for scc rds"
+  type = map(object({
+    alarm_name          = string
+    alarm_description   = string
+    comparison_operator = string
+    evaluation_periods  = number
+    threshold           = number
+    period              = number
+    unit                = optional(string)
+    namespace           = string
+    metric_name         = string
+    statistic           = string
+  }))
+
+  default = {
+    scc_rds_cpu_utilization = {
+      alarm_name          = "SccRdsCpuHigh"
+      alarm_description   = "Triggers when CPU utilization of the RDS instance exceeds 80%"
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 80
+      period              = 60
+      unit                = "Percent"
+      namespace           = "AWS/RDS"
+      metric_name         = "CPUUtilization"
+      statistic           = "Average"
+    }
+    scc_rds_connection = {
+      alarm_name          = "SccRdsTooManyConnections"
+      alarm_description   = "Triggers when connections to the RDS instance exceed 100"
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 100
+      period              = 60
+      unit                = "Count"
+      namespace           = "AWS/RDS"
+      metric_name         = "DatabaseConnections"
+      statistic           = "Average"
+    }
+    scc_rds_freeable_memory = {
+      alarm_name          = "SccRdsFreeableMemoryLow"
+      alarm_description   = "Triggers when freeable memory of the RDS instance is less than 500MB"
+      comparison_operator = "LessThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 500000000
+      period              = 60
+      unit                = "Bytes"
+      namespace           = "AWS/RDS"
+      metric_name         = "FreeableMemory"
+      statistic           = "Average"
+    }
+    scc_rds_write_latency = {
+      alarm_name          = "SccRdsWriteLatencyHigh"
+      alarm_description   = "Triggers when write latency of the RDS instance exceeds 100ms"
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 0.1
+      period              = 60
+      unit                = "Seconds"
+      namespace           = "AWS/RDS"
+      metric_name         = "WriteLatency"
+      statistic           = "Average"
+    }
+    scc_rds_read_latency = {
+      alarm_name          = "SccRdsReadLatencyHigh"
+      alarm_description   = "Triggers when read latency of the RDS instance exceeds 50ms"
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 0.05
+      period              = 60
+      unit                = "Seconds"
+      namespace           = "AWS/RDS"
+      metric_name         = "ReadLatency"
+      statistic           = "Average"
+    }
+    scc_rds_db_load_non_cpu = {
+      alarm_name          = "SccRdsLoadNonCpuHigh"
+      alarm_description   = "Triggers when average active sessions blocked by non-cpu event exceeds 1"
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      evaluation_periods  = 1
+      threshold           = 1
+      period              = 60
+      unit                = null
+      namespace           = "AWS/RDS"
+      metric_name         = "DBLoadNonCPU"
+      statistic           = "Average"
+    }
+  }
+}
--- infra/terraform/monitoring/cloudwatch.tf
+++ infra/terraform/monitoring/cloudwatch.tf
@@ -1,14 +1,16 @@
-resource "aws_cloudwatch_metric_alarm" "scc_rds_cpu_utilization" {
-  alarm_name          = "scc-rds-cpu-high"
-  alarm_description   = "Triggers when CPU utilization of the RDS instance exceeds 80%"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = 1
-  threshold           = 80
-  period              = 60
-  unit                = "Percent"
-  namespace           = "AWS/RDS"
-  metric_name         = "CPUUtilization"
-  statistic           = "Average"
+resource "aws_cloudwatch_metric_alarm" "scc_alarms" {
+  for_each = var.alarms
+
+  alarm_name          = each.value.alarm_name
+  alarm_description   = each.value.alarm_description
+  comparison_operator = each.value.comparison_operator
+  evaluation_periods  = each.value.evaluation_periods
+  threshold           = each.value.threshold
+  period              = each.value.period
+  unit                = each.value.unit
+  namespace           = each.value.namespace
+  metric_name         = each.value.metric_name
+  statistic           = each.value.statistic
 
   actions_enabled = true
   alarm_actions = [aws_sns_topic.scc_alarm_sns_topic.arn]
