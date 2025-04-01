1c3d89a52d13c9874be62cb427e203a7e71cf728
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-01 11:37:40 +0900

    narrow permissions

--- infra/terraform/hot-updater/iam.tf
+++ infra/terraform/hot-updater/iam.tf
@@ -1,16 +1,34 @@
 resource "aws_iam_user" "hot_update_user" {
   name = "hot-update-user"
-  tags = {
-    "AKIAUH5PXDMFXDWJFIHW" = "hot_updater_deploy"
-  }
-  tags_all = {
-    "AKIAUH5PXDMFXDWJFIHW" = "hot_updater_deploy"
-  }
+}
+
+resource "aws_iam_policy" "hot_updater_s3_policy" {
+  name = "hot-updater-s3-policy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          aws_s3_bucket.prod_scc_hot_updater_storage.arn,
+          "${aws_s3_bucket.prod_scc_hot_updater_storage.arn}/*",
+          aws_s3_bucket.dev_scc_hot_updater_storage.arn,
+          "${aws_s3_bucket.dev_scc_hot_updater_storage.arn}/*"
+        ]
+      }
+    ]
+  })
 }
 
 resource "aws_iam_user_policy_attachment" "hot_update_s3" {
   user       = aws_iam_user.hot_update_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = aws_iam_policy.hot_updater_s3_policy.arn
 }
 
 resource "aws_iam_role" "hot_updater_edge" {
