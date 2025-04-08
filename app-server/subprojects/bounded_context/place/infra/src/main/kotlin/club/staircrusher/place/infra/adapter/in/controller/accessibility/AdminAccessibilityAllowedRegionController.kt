28ba8a0b43f3d0190e61fc2c002e76ff584e86b8
Jason Yoo <jasonyoo0515@gmail.com>
2025-04-08 18:52:22 +0900

    chore: temp admin auth

--- app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/AdminAccessibilityAllowedRegionController.kt
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/AdminAccessibilityAllowedRegionController.kt
@@ -5,6 +5,7 @@ import club.staircrusher.admin_api.spec.dto.AccessibilityAllowedRegionDTO
 import club.staircrusher.admin_api.spec.dto.CreateAccessibilityAllowedRegionRequestDTO
 import club.staircrusher.place.application.port.`in`.accessibility.CreateAccessibilityAllowedRegionUseCase
 import club.staircrusher.place.application.port.out.accessibility.persistence.AccessibilityAllowedRegionRepository
+import club.staircrusher.spring_web.security.admin.SccAdminAuthentication
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,23 +19,34 @@ class AdminAccessibilityAllowedRegionController(
     private val accessibilityAllowedRegionRepository: AccessibilityAllowedRegionRepository,
 ) {
     @PostMapping("/admin/accessibilityAllowedRegions")
-    fun createAccessibilityAllowedRegion(@RequestBody request: CreateAccessibilityAllowedRegionRequestDTO) {
+    fun createAccessibilityAllowedRegion(
+        @RequestBody request: CreateAccessibilityAllowedRegionRequestDTO,
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ) {
         createAccessibilityAllowedRegionUseCase.handle(request.name, request.boundaryVertices.map { it.toModel() })
     }
 
     @GetMapping("/admin/accessibilityAllowedRegions")
-    fun listAllAccessibilityAllowedRegions(): List<AccessibilityAllowedRegionDTO> {
+    fun listAllAccessibilityAllowedRegions(
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ): List<AccessibilityAllowedRegionDTO> {
         return accessibilityAllowedRegionRepository.findAll().sortedByDescending { it.createdAt }
             .map { it.toDTO() }
     }
 
     @GetMapping("/admin/accessibilityAllowedRegions/{regionId}")
-    fun getAccessibilityAllowedRegion(@PathVariable regionId: String): AccessibilityAllowedRegionDTO {
+    fun getAccessibilityAllowedRegion(
+        @PathVariable regionId: String,
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ): AccessibilityAllowedRegionDTO {
         return accessibilityAllowedRegionRepository.findById(regionId).get().toDTO()
     }
 
     @DeleteMapping("/admin/accessibilityAllowedRegions/{regionId}")
-    fun deleteAccessibilityAllowedRegion(@PathVariable regionId: String) {
+    fun deleteAccessibilityAllowedRegion(
+        @PathVariable regionId: String,
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ) {
         return accessibilityAllowedRegionRepository.deleteById(regionId)
     }
 }
