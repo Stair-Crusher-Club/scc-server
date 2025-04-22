6217ca8b4371d123a54c92fe1d51f96241b6cc0d
Jason Yoo <jasonyoo0515@gmail.com>
2025-04-22 14:49:46 +0900

    chore: rollback coroutine version

--- app-server/subprojects/bounded_context/misc/infra/build.gradle.kts
+++ app-server/subprojects/bounded_context/misc/infra/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     implementation(libs.aws.sdk.rekognition)
     runtimeOnly(libs.aws.sdk.sts) // IRSA를 사용하기 위해서 필요함
     implementation("org.springframework:spring-webflux")
-    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.jdk8)
 
     integrationTestImplementation(projects.crossCuttingConcern.test.springIt)
 }
