1ed2176beed53745126accdafea3b3a0ada2daec
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-24 00:42:02 +0900

    fix test

--- app-server/subprojects/bounded_context/place/infra/src/integrationTest/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityTest.kt
+++ app-server/subprojects/bounded_context/place/infra/src/integrationTest/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityTest.kt
@@ -1,6 +1,5 @@
 package club.staircrusher.place.infra.adapter.`in`.controller.accessibility
 
-import club.staircrusher.api.spec.dto.AccessibilityReportReason
 import club.staircrusher.api.spec.dto.ReportAccessibilityPostRequest
 import club.staircrusher.place.application.port.out.accessibility.SlackService
 import club.staircrusher.place.application.port.out.accessibility.persistence.AccessibilityReportRepository
@@ -29,7 +28,7 @@ class ReportAccessibilityTest : AccessibilityITBase() {
         val user = accessibilityResult.user
 
         val params =
-            ReportAccessibilityPostRequest(placeId = place.id, reason = AccessibilityReportReason.INACCURATE_INFO)
+            ReportAccessibilityPostRequest(placeId = place.id, reason = "INACCURATE_INFO")
         mvc
             .sccRequest("/reportAccessibility", params, user)
             .andExpect {
@@ -49,7 +48,7 @@ class ReportAccessibilityTest : AccessibilityITBase() {
         val place = accessibilityResult.place
         val user = accessibilityResult.user
 
-        val reason = AccessibilityReportReason.BAD_USER
+        val reason = "BAD_USER"
         val detail = "상세 내용"
         val params = ReportAccessibilityPostRequest(placeId = place.id, reason = reason, detail = detail)
 
