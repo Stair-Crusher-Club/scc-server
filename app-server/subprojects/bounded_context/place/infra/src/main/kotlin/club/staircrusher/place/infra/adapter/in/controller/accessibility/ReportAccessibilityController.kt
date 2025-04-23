d497715ab2f966cff8c93ca298d406f57c3b3817
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-24 00:33:16 +0900

    fix

--- app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityController.kt
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityController.kt
@@ -1,8 +1,8 @@
 package club.staircrusher.place.infra.adapter.`in`.controller.accessibility
 
-import club.staircrusher.api.spec.dto.AccessibilityReportReason
 import club.staircrusher.api.spec.dto.ReportAccessibilityPostRequest
 import club.staircrusher.place.application.port.`in`.accessibility.ReportAccessibilityUseCase
+import club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason
 import club.staircrusher.spring_web.security.app.SccAppAuthentication
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -19,10 +19,10 @@ class ReportAccessibilityController(
         authentication: SccAppAuthentication,
     ): ResponseEntity<Unit> {
         val reason = when (request.reason) {
-            AccessibilityReportReason.INACCURATE_INFO -> club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason.InaccurateInfo
-            AccessibilityReportReason.CLOSED -> club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason.Closed
-            AccessibilityReportReason.BAD_USER -> club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason.BadUser
-            null -> club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason.None
+            "INACCURATE_INFO", "틀린 정보가 있어요" -> AccessibilityReportReason.InaccurateInfo
+            "CLOSED", "폐점된 곳이에요" -> AccessibilityReportReason.Closed
+            "BAD_USER", "이 정복자를 차단할래요" -> AccessibilityReportReason.BadUser
+            else -> AccessibilityReportReason.None
         }
         reportAccessibilityUseCase.handle(
             request.placeId,
