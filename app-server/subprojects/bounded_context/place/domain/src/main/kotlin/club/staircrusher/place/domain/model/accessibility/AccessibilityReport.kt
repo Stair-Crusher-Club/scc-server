4fefd86d7eb31e38738a99803d8d57290ce970eb
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-04-23 22:07:58 +0900

    cilint

--- app-server/subprojects/bounded_context/place/domain/src/main/kotlin/club/staircrusher/place/domain/model/accessibility/AccessibilityReport.kt
+++ app-server/subprojects/bounded_context/place/domain/src/main/kotlin/club/staircrusher/place/domain/model/accessibility/AccessibilityReport.kt
@@ -5,7 +5,6 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import java.time.Instant
 
 @Entity
 @Table(name = "accessibility_report")
