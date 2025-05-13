889c488e0df68686bef4b57164c9866e13629a76
Jason Yoo <jasonyoo0515@gmail.com>
2025-05-13 14:20:07 +0900

    chore: unify java version

--- app-server/build.gradle.kts
+++ app-server/build.gradle.kts
@@ -53,8 +53,8 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
 
     repositories {
@@ -82,7 +82,7 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
+            jvmTarget = "19"
         }
     }
 
