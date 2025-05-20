7f345fe5ea1ec6eb925df1c3443bedfd52555ff1
Jason Yoo <jasonyoo0515@gmail.com>
2025-05-20 14:32:22 +0900

    JWT 구조 변경 (#571)
    feat: update scc jwt

--- app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccTokenManager.kt
+++ app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/SccTokenManager.kt
@@ -1,52 +1,54 @@
-package club.staircrusher.stdlib.token
+package club.staircrusher.spring_web.security
 
+import club.staircrusher.stdlib.clock.SccClock
+import club.staircrusher.stdlib.di.annotation.Component
+import club.staircrusher.stdlib.token.TokenManager
+import club.staircrusher.stdlib.token.TokenVerificationException
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import club.staircrusher.stdlib.di.annotation.Component
 import com.auth0.jwt.exceptions.AlgorithmMismatchException
 import com.auth0.jwt.exceptions.InvalidClaimException
 import com.auth0.jwt.exceptions.JWTDecodeException
 import com.auth0.jwt.exceptions.SignatureVerificationException
 import com.auth0.jwt.exceptions.TokenExpiredException
 import com.fasterxml.jackson.databind.exc.MismatchedInputException
-import java.time.Clock
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.time.Duration
-import java.util.Date
 import kotlin.reflect.KClass
 
 @Component
-class JWTTokenManager(
-    secret: String = "secret",
-    private val clock: Clock,
+class SccTokenManager(
+    sccTokenProperties: SccTokenProperties,
 ) : TokenManager {
-    companion object {
-        private val TOKEN_EXPIRATION = Duration.ofDays(365)
-    }
-
     private val objectMapper = jacksonObjectMapper()
 
-    private val jwtAlgorithm = Algorithm.HMAC512(secret)
+    private val jwtAlgorithm = Algorithm.HMAC512(sccTokenProperties.secret)
+    private val oldJwtAlgorithm = Algorithm.HMAC512(sccTokenProperties.oldSecret)
     private val bodyKey = "_b"
     private val issuer = "our-map-server"
 
     private val verifier = JWT.require(jwtAlgorithm)
         .withIssuer(issuer)
         .build()
+    private val oldVerifier = JWT.require(oldJwtAlgorithm)
+        .withIssuer(issuer)
+        .build()
 
     override fun issueToken(content: Any): String {
+        val expiresAt = SccClock.instant() + TOKEN_EXPIRATION
         return JWT.create()
             .withIssuer(issuer)
             .withClaim(bodyKey, objectMapper.writeValueAsString(content))
-            .withExpiresAt(Date((clock.instant() + TOKEN_EXPIRATION).toEpochMilli()))
+            .withExpiresAt(expiresAt)
             .sign(jwtAlgorithm)
     }
 
     override fun issueToken(content: Any, expiresAfter: Duration): String {
+        val expiresAt = SccClock.instant() + expiresAfter
         return JWT.create()
             .withIssuer(issuer)
             .withClaim(bodyKey, objectMapper.writeValueAsString(content))
-            .withExpiresAt(Date((clock.instant() + expiresAfter).toEpochMilli()))
+            .withExpiresAt(expiresAt)
             .sign(jwtAlgorithm)
     }
 
@@ -54,11 +56,17 @@ class JWTTokenManager(
     override fun <T : Any> verify(token: String, contentClass: KClass<T>): T {
         val jwt = try {
             verifier.verify(token)
+        } catch (_: SignatureVerificationException) {
+            // 하위 호환성 맞춰주기
+            try {
+                oldVerifier.verify(token)
+            } catch (e: SignatureVerificationException) {
+                throw TokenVerificationException(e.message ?: "")
+            }
         } catch (t: Throwable) {
             when (t) {
                 is JWTDecodeException,
                 is AlgorithmMismatchException,
-                is SignatureVerificationException,
                 is TokenExpiredException,
                 is InvalidClaimException -> throw TokenVerificationException(t.message ?: "")
                 else -> throw t
@@ -69,5 +77,9 @@ class JWTTokenManager(
         } catch (e: MismatchedInputException) {
             throw TokenVerificationException(e.message ?: "")
         }
+    }
+
+    companion object {
+        private val TOKEN_EXPIRATION = Duration.ofDays(365)
     }
 }
