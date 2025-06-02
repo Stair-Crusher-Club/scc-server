8bfdf7352c409f7008a264bbeebcd9abd69b7dde
Jason Yoo <jasonyoo0515@gmail.com>
2025-06-02 22:51:38 +0900

    chore: udpate api spec

--- app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityController.kt
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/ReportAccessibilityController.kt
@@ -2,7 +2,6 @@ package club.staircrusher.place.infra.adapter.`in`.controller.accessibility
 
 import club.staircrusher.api.spec.dto.ReportAccessibilityPostRequest
 import club.staircrusher.place.application.port.`in`.accessibility.ReportAccessibilityUseCase
-import club.staircrusher.place.domain.model.accessibility.AccessibilityReportReason
 import club.staircrusher.spring_web.security.app.SccAppAuthentication
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,16 +17,10 @@ class ReportAccessibilityController(
         @RequestBody request: ReportAccessibilityPostRequest,
         authentication: SccAppAuthentication,
     ): ResponseEntity<Unit> {
-        val reason = when (request.reason) {
-            "INACCURATE_INFO" -> AccessibilityReportReason.InaccurateInfo
-            "CLOSED" -> AccessibilityReportReason.Closed
-            "BAD_USER" -> AccessibilityReportReason.BadUser
-            else -> AccessibilityReportReason.None
-        }
         reportAccessibilityUseCase.handle(
             request.placeId,
             authentication.principal,
-            reason,
+            request.reason.toModel(),
             request.detail,
         )
         return ResponseEntity
