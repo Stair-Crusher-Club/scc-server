765458d9abb58fbc2cde4d2d52918e56fe27c9c5
sanggggg <37951125+sanggggg@users.noreply.github.com>
2025-06-19 02:47:33 +0900

    add admin

--- app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/AdminAccessibilityController.kt
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/accessibility/AdminAccessibilityController.kt
@@ -1,14 +1,21 @@
 package club.staircrusher.place.infra.adapter.`in`.controller.accessibility
 
 import club.staircrusher.admin_api.spec.dto.AdminAccessibilityDTO
+import club.staircrusher.admin_api.spec.dto.AdminAccessibilityImageDTO
+import club.staircrusher.admin_api.spec.dto.AdminAccessibilityImageDTOInspectionResult
 import club.staircrusher.admin_api.spec.dto.AdminSearchAccessibilitiesResultDTO
+import club.staircrusher.admin_api.spec.dto.AdminSearchAccessibilityImagesResultDTO
 import club.staircrusher.admin_api.spec.dto.AdminUpdateBuildingAccessibilityRequestDTO
 import club.staircrusher.admin_api.spec.dto.AdminUpdatePlaceAccessibilityRequestDTO
+import club.staircrusher.admin_api.spec.dto.EpochMillisTimestamp
 import club.staircrusher.place.application.port.`in`.accessibility.AdminDeleteBuildingAccessibilityUseCase
 import club.staircrusher.place.application.port.`in`.accessibility.AdminDeletePlaceAccessibilityUseCase
 import club.staircrusher.place.application.port.`in`.accessibility.AdminSearchAccessibilitiesUseCase
+import club.staircrusher.place.application.port.`in`.accessibility.AdminSearchAccessibilityImagesUseCase
 import club.staircrusher.place.application.port.`in`.accessibility.AdminUpdateBuildingAccessibilityUseCase
 import club.staircrusher.place.application.port.`in`.accessibility.AdminUpdatePlaceAccessibilityUseCase
+import club.staircrusher.place.domain.model.accessibility.AccessibilityImage
+import club.staircrusher.place.domain.model.check.ImageInspectionResult
 import club.staircrusher.stdlib.util.string.emptyToNull
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -23,6 +30,7 @@ import java.time.LocalDate
 @RestController
 class AdminAccessibilityController(
     private val adminSearchAccessibilitiesUseCase: AdminSearchAccessibilitiesUseCase,
+    private val adminSearchAccessibilityImagesUseCase: AdminSearchAccessibilityImagesUseCase,
     private val adminUpdatePlaceAccessibilityUseCase: AdminUpdatePlaceAccessibilityUseCase,
     private val adminUpdateBuildingAccessibilityUseCase: AdminUpdateBuildingAccessibilityUseCase,
     private val adminDeletePlaceAccessibilityUseCase: AdminDeletePlaceAccessibilityUseCase,
@@ -55,6 +63,85 @@ class AdminAccessibilityController(
                         registeredUserName = it.buildingAccessibilityRegisteredUser?.nickname,
                     )
 
+                )
+            },
+            cursor = result.cursor,
+        )
+    }
+
+    @GetMapping("/admin/accessibilty-images/search")
+    fun searchAccessibilityImages(
+        @RequestParam(required = false) inspectionResultType: String?,
+        @RequestParam(required = false) cursor: String?,
+        @RequestParam(required = false) limit: Int?,
+    ): AdminSearchAccessibilityImagesResultDTO {
+        val result = adminSearchAccessibilityImagesUseCase.handle(
+            inspectionResultType = inspectionResultType,
+            cursorValue = cursor,
+            limit = limit,
+        )
+        return AdminSearchAccessibilityImagesResultDTO(
+            items = result.items.map { image ->
+                val accessibilityType = when (image.accessibilityType) {
+                    AccessibilityImage.AccessibilityType.Place -> AdminAccessibilityImageDTO.AccessibilityType.PLACE
+                    AccessibilityImage.AccessibilityType.Building -> AdminAccessibilityImageDTO.AccessibilityType.BUILDING
+                }
+
+                val imageType = image.imageType?.let { imageType ->
+                    when (imageType) {
+                        AccessibilityImage.ImageType.Entrance -> AdminAccessibilityImageDTO.ImageType.ENTRANCE
+                        AccessibilityImage.ImageType.Elevator -> AdminAccessibilityImageDTO.ImageType.ELEVATOR
+                    }
+                }
+
+                val inspectionResult = image.inspectionResult?.let {
+                    when (it) {
+                        is ImageInspectionResult.Visible -> {
+                            val objects = it.objects.map { obj ->
+                                when (obj) {
+                                    ImageInspectionResult.DetectedObject.Elevator -> AdminAccessibilityImageDTOInspectionResult.Objects.ELEVATOR
+                                    ImageInspectionResult.DetectedObject.Entrance -> AdminAccessibilityImageDTOInspectionResult.Objects.ENTRANCE
+                                    ImageInspectionResult.DetectedObject.Stair -> AdminAccessibilityImageDTOInspectionResult.Objects.STAIR
+                                }
+                            }
+                            val rotation = when (it.rotation) {
+                                ImageInspectionResult.Rotation.D0 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D0
+                                ImageInspectionResult.Rotation.D90 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D90
+                                ImageInspectionResult.Rotation.D180 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D180
+                                ImageInspectionResult.Rotation.D270 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D270
+                            }
+                            AdminAccessibilityImageDTOInspectionResult(
+                                type = AdminAccessibilityImageDTOInspectionResult.Type.VISIBLE,
+                                objects = objects,
+                                rotation = rotation
+                            )
+                        }
+                        is ImageInspectionResult.NotVisible -> {
+                            val rotation = when (it.rotation) {
+                                ImageInspectionResult.Rotation.D0 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D0
+                                ImageInspectionResult.Rotation.D90 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D90
+                                ImageInspectionResult.Rotation.D180 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D180
+                                ImageInspectionResult.Rotation.D270 -> AdminAccessibilityImageDTOInspectionResult.Rotation.D270
+                            }
+                            AdminAccessibilityImageDTOInspectionResult(
+                                type = AdminAccessibilityImageDTOInspectionResult.Type.NOT_VISIBLE,
+                                rotation = rotation
+                            )
+                        }
+                    }
+                }
+
+                AdminAccessibilityImageDTO(
+                    id = image.id,
+                    accessibilityId = image.accessibilityId,
+                    accessibilityType = accessibilityType,
+                    originalImageUrl = image.originalImageUrl,
+                    blurredImageUrl = image.blurredImageUrl,
+                    thumbnailUrl = image.thumbnailUrl,
+                    imageType = imageType,
+                    inspectionResult = inspectionResult,
+                    createdAt = EpochMillisTimestamp(image.createdAt.toEpochMilli()),
+                    displayOrder = image.displayOrder
                 )
             },
             cursor = result.cursor,
