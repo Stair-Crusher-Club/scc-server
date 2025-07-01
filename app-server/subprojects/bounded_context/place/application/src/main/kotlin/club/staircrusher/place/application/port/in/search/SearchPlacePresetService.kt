63ce2c98fab09e3e82d2979585eff752667dfc03
Jason Yoo <jasonyoo0515@gmail.com>
2025-07-01 12:02:59 +0900

    feat: admin api

--- app-server/subprojects/bounded_context/place/application/src/main/kotlin/club/staircrusher/place/application/port/in/search/SearchPlacePresetService.kt
+++ app-server/subprojects/bounded_context/place/application/src/main/kotlin/club/staircrusher/place/application/port/in/search/SearchPlacePresetService.kt
@@ -4,25 +4,27 @@ import club.staircrusher.place.application.port.out.search.persistence.SearchPla
 import club.staircrusher.place.domain.model.search.SearchPlacePreset
 import club.staircrusher.stdlib.di.annotation.Component
 import club.staircrusher.stdlib.domain.entity.EntityIdGenerator
+import club.staircrusher.stdlib.persistence.TransactionManager
 
 @Component
 class SearchPlacePresetService(
+    private val transactionManager: TransactionManager,
     private val searchPlacePresetRepository: SearchPlacePresetRepository,
 ) {
-    fun list(): List<SearchPlacePreset> {
-        return searchPlacePresetRepository.findAll().toList()
+    fun list() = transactionManager.doInTransaction(isReadOnly = true) {
+        searchPlacePresetRepository.findAll().toList()
     }
 
-    fun createKeywordPreset(description: String, searchText: String): SearchPlacePreset {
+    fun createKeywordPreset(description: String, searchText: String) = transactionManager.doInTransaction {
         val preset = SearchPlacePreset.ofKeyword(
             id = EntityIdGenerator.generateRandom(),
             description = description,
             searchText = searchText
         )
-        return searchPlacePresetRepository.save(preset)
+        searchPlacePresetRepository.save(preset)
     }
 
-    fun deleteById(id: String) {
+    fun deleteById(id: String) = transactionManager.doInTransaction {
         searchPlacePresetRepository.deleteById(id)
     }
 }
---
+++ app-server/subprojects/bounded_context/place/infra/src/main/kotlin/club/staircrusher/place/infra/adapter/in/controller/search/AdminSearchPlacesController.kt
@@ -0,0 +1,51 @@
+package club.staircrusher.place.infra.adapter.`in`.controller.search
+
+import club.staircrusher.admin_api.spec.dto.AdminCreateSearchPlacePresetRequestDTO
+import club.staircrusher.admin_api.spec.dto.GetSearchPreset200Response
+import club.staircrusher.place.application.port.`in`.search.SearchPlacePresetService
+import club.staircrusher.spring_web.security.admin.SccAdminAuthentication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AdminSearchPlacesController(
+    private val searchPlacePresetService: SearchPlacePresetService,
+) {
+
+    @GetMapping("/admin/places/searchPreset")
+    fun getSearchPresets(
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ) : GetSearchPreset200Response {
+        return GetSearchPreset200Response(
+            presets = searchPlacePresetService.list().map { it.toAdminDTO() }
+        )
+    }
+
+    @PostMapping("/admin/places/searchPreset")
+    fun createSearchPreset(
+        @RequestBody request: AdminCreateSearchPlacePresetRequestDTO,
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ) : ResponseEntity<Unit> {
+        searchPlacePresetService.createKeywordPreset(
+            description = request.description,
+            searchText = request.searchText
+        )
+
+        return ResponseEntity.noContent().build()
+    }
+
+    @DeleteMapping("/admin/places/searchPreset/{presetId}")
+    fun deleteSearchPreset(
+        @PathVariable presetId: String,
+        @Suppress("UnusedPrivateMember") authentication: SccAdminAuthentication,
+    ) : ResponseEntity<Unit> {
+        searchPlacePresetService.deleteById(presetId)
+
+        return ResponseEntity.noContent().build()
+    }
+}
