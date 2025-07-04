5bb8385d0748030224cbd653958a35f11aa8c8d1
Jason Yoo <jasonyoo0515@gmail.com>
2025-07-04 16:19:44 +0900

    chore: add user id idx

--- app-server/subprojects/cross_cutting_concern/infra/persistence_model/src/main/resources/db/migration/V49__alter_place_review_toilet_review.sql
+++ app-server/subprojects/cross_cutting_concern/infra/persistence_model/src/main/resources/db/migration/V49__alter_place_review_toilet_review.sql
@@ -2,5 +2,9 @@ ALTER TABLE place_review
     ADD COLUMN user_id VARCHAR(36) NOT NULL,
     ALTER COLUMN comment DROP NOT NULL;
 
+CREATE INDEX idx_place_review_user_id ON place_review(user_id);
+
 ALTER TABLE toilet_review
     ADD COLUMN user_id VARCHAR(36) NOT NULL;
+
+CREATE INDEX idx_toilet_review_user_id ON toilet_review(user_id);
