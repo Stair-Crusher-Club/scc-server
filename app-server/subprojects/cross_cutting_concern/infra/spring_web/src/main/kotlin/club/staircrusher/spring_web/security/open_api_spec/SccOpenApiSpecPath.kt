ea4ce6334821849e089a023fe96c5a077c737736
Zeniuus <jsh56son@gmail.com>
2025-07-04 14:01:26 +0900

    OpenAPI 명세를 기반으로 security config 설정하기

---
+++ app-server/subprojects/cross_cutting_concern/infra/spring_web/src/main/kotlin/club/staircrusher/spring_web/security/open_api_spec/SccOpenApiSpecPath.kt
@@ -0,0 +1,17 @@
+package club.staircrusher.spring_web.security.open_api_spec
+
+import org.springframework.http.HttpMethod
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
+
+data class SccOpenApiSpecPath(
+    val url: String,
+    val method: HttpMethod,
+    val securityTypes: List<SccOpenApiSpecSecurityType>,
+) {
+    val isIdentifiedUserOnly = SccOpenApiSpecSecurityType.IDENTIFIED in securityTypes
+
+    fun toRequestMatcher(): RequestMatcher {
+        return AntPathRequestMatcher(url, method.name())
+    }
+}
